yeison/modular

commit 94a9d50290b907b9406fe67d8c9e45296f843700

[Mojo API] Add input and output metadata getters in Mojo model API (#33830)
This PR introduces two new Mojo `Model` getters for `input` and `output`
tensor metadata respectively.

---------

Co-authored-by: Arjun Surendran <arjun@modular.com>
MOJO_MAX_ORIG_REV_ID: 56abd68fdc7ae4db2574683df9a5dd41168c6991

mojo/max/src/engine/tensor_spec.mojo
@@ -19,7 +19,7 @@ from .session import InferenceSession
 from ._tensor_spec_impl import CTensorSpec
 
 
-struct EngineTensorSpec(Stringable, Movable):
+struct EngineTensorSpec(Stringable, CollectionElement):
     """
     Describes input/output of a Max Engine Model.
     """
@@ -29,6 +29,43 @@ struct EngineTensorSpec(Stringable, Movable):
     var _session: InferenceSession
 
     alias _NewTensorSpecFnName = "M_newTensorSpec"
+
+    fn __copyinit__(inout self, other: Self):
+        """Copy constructor for Tensor Spec.
+
+        Args:
+            other: Instance of TensorSpec to be copied.
+        """
+        self._lib = other._lib
+        self._session = other._session
+
+        var rank = other.rank()
+        var name = other.get_name()
+        var shape = other.get_shape()
+
+        if shape:
+            var inner_shape = shape.value()
+            var rank = len(inner_shape)
+            self._ptr = call_dylib_func[CTensorSpec](
+                self._lib,
+                Self._NewTensorSpecFnName,
+                inner_shape.data,
+                rank,
+                other._ptr.get_dtype(self._lib),
+                name._as_ptr(),
+            )
+            _ = inner_shape ^
+        else:
+            self._ptr = call_dylib_func[CTensorSpec](
+                self._lib,
+                Self._NewTensorSpecFnName,
+                CTensorSpec.ptr_type(),
+                CTensorSpec.get_dynamic_rank_value(self._lib),
+                other._ptr.get_dtype(self._lib),
+                name._as_ptr(),
+            )
+        _ = name
+        _ = shape
 
     fn __moveinit__(inout self, owned existing: Self):
         """Move constructor for Tensor Spec.
@@ -221,6 +258,40 @@ struct EngineTensorSpec(Stringable, Movable):
         """
         return self._ptr.get_name(self._lib)
 
+    fn get_dtype(self) -> DType:
+        """Gets the DType of tensor corresponding to spec.
+
+        Returns:
+            DType of the Tensor.
+        """
+        return self._ptr.get_dtype(self._lib).to_dtype()
+
+    fn get_shape(self) -> Optional[List[Optional[Int]]]:
+        """Gets the shape of tensor corresponding to spec.
+
+        Returns:
+            Shape of the Tensor. Returns None if rank is dynamic.
+        """
+        var rank_or = self.rank()
+        if not rank_or:
+            return None
+
+        var shape_list = List[Optional[Int]]()
+        var rank = rank_or.value()
+        for i in range(rank):
+            var dim: Optional[Int]
+            try:
+                dim = self[i]
+            except err:
+                abort("unreachable condition")
+
+            if not dim:
+                shape_list.push_back(None)
+            else:
+                shape_list.push_back(dim.value())
+
+        return shape_list
+
     fn __str__(self) -> String:
         """Gets the String representation of Spec.
 
@@ -230,24 +301,19 @@ struct EngineTensorSpec(Stringable, Movable):
         var _repr: String = "{name="
         _repr += self.get_name()
         _repr += ", spec="
-        var rank_or = self.rank()
-        if not rank_or:
-            _repr += "None x "
-            _repr += str(self._ptr.get_dtype(self._lib).to_dtype())
-        else:
-            var rank = rank_or.value()
-            for i in range(rank):
-                var dim: Optional[Int]
-                try:
-                    dim = self[i]
-                except err:
-                    abort("unreachable condition")
 
-                if not dim:
-                    _repr += "-1x"
+        var shape_list = self.get_shape()
+
+        if not shape_list:
+            _repr += "None x "
+        else:
+            for dim in shape_list.value():
+                if not dim[]:
+                    _repr += "-1"
                 else:
-                    _repr += str(dim.value()) + "x"
-            _repr += str(self._ptr.get_dtype(self._lib).to_dtype())
+                    _repr += str(dim[].value())
+                _repr += "x"
+        _repr += str(self.get_dtype())
         _repr += "}"
         return _repr
 

mojo/max/src/engine/_compilation.mojo
@@ -312,6 +312,8 @@ struct CCompiledModel:
     var ptr: DTypePointer[DType.invalid]
 
     alias FreeCompiledModelFnName = "M_freeCompiledModel"
+    alias GetModelInputSpecByNameFnName = "M_getModelInputSpecByName"
+    alias GetModelOutputSpecByNameFnName = "M_getModelOutputSpecByName"
     alias GetNumInputsFnName = "M_getNumModelInputs"
     alias GetNumOutputsFnName = "M_getNumModelOutputs"
 
@@ -337,6 +339,44 @@ struct CCompiledModel:
             raise Error(status.__str__())
         return num_outputs
 
+    fn get_model_input_spec_by_name(
+        self,
+        tensor_name: String,
+        borrowed lib: DLHandle,
+        owned session: InferenceSession,
+    ) raises -> EngineTensorSpec:
+        """Gets the input spec of the model by name."""
+        var status = Status(lib)
+        var input_spec = call_dylib_func[CTensorSpec](
+            lib,
+            Self.GetModelInputSpecByNameFnName,
+            self,
+            tensor_name._as_ptr(),
+            status.ptr,
+        )
+        if status:
+            raise Error(status.__str__())
+        return EngineTensorSpec(input_spec, lib, session.copy())
+
+    fn get_model_output_spec_by_name(
+        self,
+        tensor_name: String,
+        borrowed lib: DLHandle,
+        owned session: InferenceSession,
+    ) raises -> EngineTensorSpec:
+        """Gets the output spec of the model by name."""
+        var status = Status(lib)
+        var output_spec = call_dylib_func[CTensorSpec](
+            lib,
+            Self.GetModelOutputSpecByNameFnName,
+            self,
+            tensor_name._as_ptr(),
+            status.ptr,
+        )
+        if status:
+            raise Error(status.__str__())
+        return EngineTensorSpec(output_spec, lib, session.copy())
+
     fn free(self, borrowed lib: DLHandle):
         call_dylib_func(lib, Self.FreeCompiledModelFnName, self)
 
@@ -350,8 +390,6 @@ struct CompiledModel:
     var session: InferenceSession
 
     alias CompileModelFnName = "M_compileModelSync"
-    alias GetModelInputSpecByNameFnName = "M_getModelInputSpecByName"
-    alias GetModelOutputSpecByNameFnName = "M_getModelOutputSpecByName"
 
     fn __moveinit__(inout self, owned existing: Self):
         self.ptr = exchange[CCompiledModel](
@@ -393,6 +431,32 @@ struct CompiledModel:
         for i in range(len(names)):
             name_vec.push_back(names[i])
         return name_vec
+
+    fn get_model_input_metadata(self) raises -> List[EngineTensorSpec]:
+        """Get the metadata for inputs of the model."""
+        var input_metadata = List[EngineTensorSpec]()
+        var input_tensor_names = self.get_model_input_names()
+        input_metadata.reserve(len(input_tensor_names))
+
+        for input_tensor_name in input_tensor_names:
+            var input_spec = self.ptr.get_model_input_spec_by_name(
+                input_tensor_name[], self.lib, self.session
+            )
+            input_metadata.push_back(input_spec ^)
+        return input_metadata
+
+    fn get_model_output_metadata(self) raises -> List[EngineTensorSpec]:
+        """Get the metadata for outputs of the model."""
+        var output_metadata = List[EngineTensorSpec]()
+        var output_tensor_names = self.get_model_output_names()
+        output_metadata.reserve(len(output_tensor_names))
+
+        for output_tensor_name in output_tensor_names:
+            var output_spec = self.ptr.get_model_output_spec_by_name(
+                output_tensor_name[], self.lib, self.session
+            )
+            output_metadata.push_back(output_spec ^)
+        return output_metadata
 
     fn borrow_ptr(self) -> CCompiledModel:
         return self.ptr

mojo/max/src/engine/model.mojo
@@ -15,6 +15,7 @@ from sys.ffi import DLHandle
 from ._utils import call_dylib_func, exchange
 from ._status import Status
 from .tensor_map import CTensorMap
+from tensor import TensorSpec
 from sys.intrinsics import _mlirtype_is_eq
 from utils import StringRef
 
@@ -328,6 +329,24 @@ struct Model:
             Output names of the model.
         """
         return self._compiled_model.get_model_output_names()
+
+    fn get_model_input_metadata(self) raises -> List[EngineTensorSpec]:
+        """Get metadata about the model's input tensors, as a list of EngineTensorSpec
+        objects.
+
+        Returns:
+            Metadata list of the model's input tensors.
+        """
+        return self._compiled_model.get_model_input_metadata()
+
+    fn get_model_output_metadata(self) raises -> List[EngineTensorSpec]:
+        """Get metadata about the model's output tensors, as a list of EngineTensorSpec
+        objects.
+
+        Returns:
+            Metadata list of the model's output tensors.
+        """
+        return self._compiled_model.get_model_output_metadata()
 
     fn __del__(owned self):
         """Destructor for Model."""

commit 41179d84f76b7f3c7c299e6af9c109c94ff85933

[MAX][Mojo API] Revamp EngineTensorSpec copy constructor and add dynamic dim and rank unit tests (#34741)
This PR fixes some lifetime bugs in `EngineTensorSpec`'s new
`__copyinit__` copy constructor introduced in #33830.

MOJO_MAX_ORIG_REV_ID: 2a82eefa7101f1f16ee8ec0ce2996b311f90335e

mojo/max/src/engine/tensor_spec.mojo
@@ -36,36 +36,13 @@ struct EngineTensorSpec(Stringable, CollectionElement):
         Args:
             other: Instance of TensorSpec to be copied.
         """
-        self._lib = other._lib
-        self._session = other._session
-
-        var rank = other.rank()
-        var name = other.get_name()
-        var shape = other.get_shape()
-
-        if shape:
-            var inner_shape = shape.value()
-            var rank = len(inner_shape)
-            self._ptr = call_dylib_func[CTensorSpec](
-                self._lib,
-                Self._NewTensorSpecFnName,
-                inner_shape.data,
-                rank,
-                other._ptr.get_dtype(self._lib),
-                name._as_ptr(),
-            )
-            _ = inner_shape ^
-        else:
-            self._ptr = call_dylib_func[CTensorSpec](
-                self._lib,
-                Self._NewTensorSpecFnName,
-                CTensorSpec.ptr_type(),
-                CTensorSpec.get_dynamic_rank_value(self._lib),
-                other._ptr.get_dtype(self._lib),
-                name._as_ptr(),
-            )
-        _ = name
-        _ = shape
+        self = Self(
+            other.get_name(),
+            other.get_shape(),
+            other.get_dtype(),
+            other._lib,
+            other._session.copy(),
+        )
 
     fn __moveinit__(inout self, owned existing: Self):
         """Move constructor for Tensor Spec.
@@ -137,6 +114,46 @@ struct EngineTensorSpec(Stringable, CollectionElement):
     fn __init__(
         inout self,
         name: String,
+        shape: Optional[List[Optional[Int]]],
+        dtype: DType,
+        lib: DLHandle,
+        owned session: InferenceSession,
+    ):
+        """Creates an instance of EngineTensorSpec.
+        Do not use this function directly.
+        Use functions from InferenceSession to create EngineTensorSpec.
+        Note that this almost similar to another constructor.
+        This accepts Int instead of Int64 shape types.
+
+        Args:
+            name: Name of tensor.
+            shape: Shape of the tensor.
+            dtype: DataType of the tensor.
+            lib: Handle to the library.
+            session: Copy of InferenceSession from which this instance
+                     was created.
+        """
+
+        if not shape:
+            self = Self(
+                name,
+                Optional[List[Optional[Int64]]](None),
+                dtype,
+                lib,
+                session.copy(),
+            )
+        else:
+            var casted_shape = List[Optional[Int64]]()
+            for dim in shape.value():
+                if not dim[]:
+                    casted_shape.push_back(None)
+                else:
+                    casted_shape.push_back(Int64(dim[].value()))
+            self = Self(name, casted_shape, dtype, lib, session.copy())
+
+    fn __init__(
+        inout self,
+        name: String,
         shape: Optional[List[Optional[Int64]]],
         dtype: DType,
         lib: DLHandle,
@@ -145,6 +162,8 @@ struct EngineTensorSpec(Stringable, CollectionElement):
         """Creates an instance of EngineTensorSpec.
         Do not use this function directly.
         Use functions from InferenceSession to create EngineTensorSpec.
+        Note that this almost similar to another constructor.
+        This accepts Int64 instead of Int shape types.
 
         Args:
             name: Name of tensor.

mojo/max/test/engine/test_tensor_spec.mojo
@@ -4,7 +4,7 @@
 #
 # ===----------------------------------------------------------------------=== #
 # UNSUPPORTED: windows
-# RUN: %mojo -I %engine_pkg_dir %s | FileCheck %s
+# RUN: %mojo -debug-level full %s
 
 from max.engine import (
     InferenceSession,
@@ -12,27 +12,23 @@ from max.engine import (
 )
 from tensor import TensorSpec
 from collections.optional import Optional
+from testing import assert_false, assert_equal, assert_true
 
 
-fn test_tensor_spec() raises:
-    # CHECK: test_tensor_spec
-    print("====test_tensor_spec")
+fn test_tensor_spec_basic() raises:
+    print("==== test_tensor_spec_basic")
 
     var session = InferenceSession()
     var spec = TensorSpec(DType.float32, 1, 2, 3)
     var engine_spec = session.get_as_engine_tensor_spec("tensor", spec)
 
-    # CHECK: 1
-    print(engine_spec[0].value())
+    assert_equal(engine_spec[0].value(), 1)
 
-    # CHECK: tensor
-    print(engine_spec.get_name())
+    assert_equal(engine_spec.get_name(), "tensor")
 
-    # CHECK: {name=tensor, spec=1x2x3xfloat32}
-    print(str(engine_spec))
+    assert_equal(str(engine_spec), "{name=tensor, spec=1x2x3xfloat32}")
 
-    # CHECK: True
-    print(engine_spec.get_as_tensor_spec() == spec)
+    assert_equal(engine_spec.get_as_tensor_spec(), spec)
 
     var dynamic_dim_shape = List[Optional[Int64]]()
     dynamic_dim_shape.push_back(None)
@@ -42,31 +38,96 @@ fn test_tensor_spec() raises:
         "tensor", dynamic_dim_shape, DType.float32
     )
 
-    # CHECK: False
-    print(dynamic_dim_spec[0].__bool__())
+    assert_false(dynamic_dim_spec[0])
 
-    # CHECK: 3
-    print(dynamic_dim_spec.rank().value())
+    assert_equal(dynamic_dim_spec.rank().value(), 3)
 
-    # CHECK: True
-    print(dynamic_dim_spec.has_rank())
+    assert_true(dynamic_dim_spec.has_rank())
 
-    # CHECK: {name=tensor, spec=-1x1x2xfloat32}
-    print(str(dynamic_dim_spec))
+    assert_equal(str(dynamic_dim_spec), "{name=tensor, spec=-1x1x2xfloat32}")
 
     var dynamic_rank_spec = session.get_as_engine_tensor_spec(
         "tensor", None, DType.float32
     )
 
-    # CHECK: False
-    print(dynamic_rank_spec.rank().__bool__())
+    assert_false(dynamic_rank_spec.rank())
 
-    # CHECK: False
-    print(dynamic_rank_spec.has_rank())
+    assert_false(dynamic_rank_spec.has_rank())
 
-    # CHECK: {name=tensor, spec=None x float32}
-    print(str(dynamic_rank_spec))
+    assert_equal(str(dynamic_rank_spec), "{name=tensor, spec=None x float32}")
+
+
+fn test_engine_tensor_spec_static_dim_copy() raises:
+    print("==== test_engine_tensor_spec_static_dim_copy")
+
+    var session = InferenceSession()
+    var spec = TensorSpec(DType.float32, 1, 2, 3)
+    var static_engine_spec = session.get_as_engine_tensor_spec("tensor", spec)
+
+    var static_engine_spec_copy = static_engine_spec
+
+    assert_equal(
+        static_engine_spec_copy[0].value(), static_engine_spec[0].value()
+    )
+
+    assert_equal(
+        static_engine_spec_copy.get_name(), static_engine_spec.get_name()
+    )
+
+    assert_equal(
+        str(static_engine_spec_copy), "{name=tensor, spec=1x2x3xfloat32}"
+    )
+
+    assert_equal(static_engine_spec_copy.get_as_tensor_spec(), spec)
+
+
+fn test_engine_tensor_spec_dynamic_dim_copy() raises:
+    print("==== test_engine_tensor_spec_dynamic_dim_copy")
+
+    var session = InferenceSession()
+    var dynamic_dim_shape = List[Optional[Int64]]()
+    dynamic_dim_shape.push_back(None)
+    dynamic_dim_shape.push_back(Int64(1))
+    dynamic_dim_shape.push_back(Int64(2))
+    var dynamic_dim_spec = session.get_as_engine_tensor_spec(
+        "tensor", dynamic_dim_shape, DType.float32
+    )
+
+    var dynamic_dim_shape_spec_copy = dynamic_dim_spec
+
+    assert_equal(
+        dynamic_dim_shape_spec_copy[0].__bool__(),
+        dynamic_dim_spec[0].__bool__(),
+    )
+
+    assert_equal(
+        dynamic_dim_shape_spec_copy.rank().value(),
+        dynamic_dim_spec.rank().value(),
+    )
+
+    assert_equal(
+        dynamic_dim_shape_spec_copy.has_rank(), dynamic_dim_spec.has_rank()
+    )
+
+    assert_equal(
+        str(dynamic_dim_shape_spec_copy), "{name=tensor, spec=-1x1x2xfloat32}"
+    )
+
+    var dynamic_rank_spec = session.get_as_engine_tensor_spec(
+        "tensor", None, DType.float32
+    )
+    var dynamic_rank_spec_copy = dynamic_rank_spec
+
+    assert_false(dynamic_rank_spec_copy.rank())
+
+    assert_false(dynamic_rank_spec_copy.has_rank())
+
+    assert_equal(
+        str(dynamic_rank_spec_copy), "{name=tensor, spec=None x float32}"
+    )
 
 
 fn main() raises:
-    test_tensor_spec()
+    test_tensor_spec_basic()
+    test_engine_tensor_spec_static_dim_copy()
+    test_engine_tensor_spec_dynamic_dim_copy()