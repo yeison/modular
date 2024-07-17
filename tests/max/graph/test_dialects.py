53061ff1d36041c2f359018dfea7c921a1ff8301
Brendan Duke <bduke@modular.com>
2024-07-17 16:07:16 -0400

    [Graph API] Add MO and RMO Python bindings (#43593)
    Add the MO and RMO Python bindings.
    
    [Docs](https://mlir.llvm.org/docs/Bindings/Python/#integration-with-ods).
    
    Co-authored-by: Stef Lindall <bethebunny@gmail.com>
    MAX_GRAPH_API_ORIG_REV_ID: b080da5b930bd99803d988a3d8c59e45ff538a6d

---
+++ tests/max/graph/test_dialects.py
@@ -0,0 +1,14 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+"""Test the Modular dialects needed by MAX graph."""
+
+
+def test_mo():
+    from max.graph.mlir.dialects import mo
+
+
+def test_rmo():
+    from max.graph.mlir.dialects import rmo
