179a3857322ea34a7e5ccbd0e0a34ae43d207ac9
akirchhoff-modular <akirchhoff@modular.com>
2024-08-12 13:38:36 -0700

    [GraphAPI] Fix MyPy error in casting ops (#45021)
    Fixes this MyPy error:
    
    ```
    SDK/lib/GraphAPI/python/max/graph/ops/casting.py:21: error: Incompatible types
    in assignment (expression has type "StringAttr", variable has type "str")
    [assignment]
            message = mlir.StringAttr.get(message)
                      ^~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ```
    
    MAX_GRAPH_API_ORIG_REV_ID: af00759f030faf0cf5ba3b81fb7b10e2a691aff7

--- max/graph/ops/casting.py
+++ max/graph/ops/casting.py
@@ -15,15 +15,16 @@ from ..type import DType, ShapeLike, StaticDim, dim
 
 
 def rebind(x: ValueLike, shape: ShapeLike, message: str):
-    # TODO(MSDK-662): add checks to ensure that statically known dims are rebound in a way to keep the size the same.
+    # TODO(MSDK-662): Add checks to ensure that statically known dims are
+    # rebound in a way to keep the size the same.
     v = GraphValue(x)
     dims = [dim(d) for d in shape]
-    message = mlir.StringAttr.get(message)
+    message_attr = mlir.StringAttr.get(message)
     return Graph.current._add_op(
         rmo.rebind_tensor_shape,
         TensorType(v.dtype, dims).to_mlir(),
         v,
-        message=message,
+        message=message_attr,
     )[0]
 
 
