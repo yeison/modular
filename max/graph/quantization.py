81b84970b940849644bd82a286eeff379657b3f1
akirchhoff-modular <akirchhoff@modular.com>
2024-08-20 09:43:52 -0700

    [GraphAPI] Add block parameters to QuantizationEncoding (#45521)
    These are needed by the dequantize op, and possibly will be needed
    elsewhere in the future, too.
    
    MAX_GRAPH_API_ORIG_REV_ID: 20183965f2c2f513fdbea6372df7c270005b374c

--- max/graph/quantization.py
+++ max/graph/quantization.py
@@ -5,7 +5,16 @@
 # ===----------------------------------------------------------------------=== #
 """Quantization support for MAX Graph."""
 
+from dataclasses import dataclass
 import enum
+from typing import Dict
+
+
+# Can't put this directly on the enum because it then becomes unhashable.
+@dataclass(frozen=True)
+class BlockParameters:
+    elements_per_block: int
+    block_size: int
 
 
 class QuantizationEncoding(enum.Enum):
@@ -15,3 +24,45 @@ class QuantizationEncoding(enum.Enum):
     Q4_K = "Q4_K"
     Q5_K = "Q5_K"
     Q6_K = "Q6_K"
+
+    @property
+    def block_parameters(self) -> BlockParameters:
+        return _BLOCK_PARAMETERS[self]
+
+    @property
+    def elements_per_block(self) -> int:
+        """Number of elements per block.
+
+        All quantization types currently supported by MAX Graph are
+        block-based: groups of a fixed number of elements are formed, and each
+        group is quantized together into a fixed-size output block.  This value
+        is the number of elements gathered into a block.
+        """
+        return self.block_parameters.elements_per_block
+
+    @property
+    def block_size(self) -> int:
+        """Number of bytes in encoded representation of block.
+
+        All quantization types currently supported by MAX Graph are
+        block-based: groups of a fixed number of elements are formed, and each
+        group is quantized together into a fixed-size output block.  This value
+        is the number of bytes resulting after encoding a single block.
+        """
+        return self.block_parameters.block_size
+
+
+_BLOCK_PARAMETERS: Dict[QuantizationEncoding, BlockParameters] = {
+    # Block: d, q (4 bits)
+    QuantizationEncoding.Q4_0: BlockParameters(32, 2 + 32 // 2),
+    # Block: d, dmin, scales, q (4 bits)
+    QuantizationEncoding.Q4_K: BlockParameters(256, 2 + 2 + 12 + 256 // 2),
+    # Block: d, dmin, scales, qh (4 bits), qs (1 bit)
+    QuantizationEncoding.Q5_K: BlockParameters(
+        256, 2 + 2 + 12 + 256 // 2 + 256 // 8
+    ),
+    # Block: ql, qh, scales, d
+    QuantizationEncoding.Q6_K: BlockParameters(
+        256, 256 // 2 + 256 // 4 + 256 // 16 + 2
+    ),
+}
