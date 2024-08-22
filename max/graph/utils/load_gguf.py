c4033ac1ba0c0b1aede73560ef033bece63ec3a5
Stef Lindall <stef@modular.com>
2024-08-21 18:10:22 -0700

    [Graph API] Attribute-based weight loading API (#45712)
    Stacked PRs:
     * #45713
     * __->__#45712
    
    --- --- ---
    
    ### [Graph API] Attribute-based weight loading API
    
    MAX_GRAPH_API_ORIG_REV_ID: 2632a0c1cb59b3f00db5c23a39d3486d2c5023e1

--- max/graph/utils/load_gguf.py
+++ max/graph/utils/load_gguf.py
@@ -5,9 +5,13 @@
 # ===----------------------------------------------------------------------=== #
 """Function for importing a GGUF checkpoint into a MAX Graph."""
 
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from os import PathLike
 import subprocess
 import sys
-from typing import Dict
+from typing import Dict, Optional, Callable
 
 # Only import gguf when used.
 try:
@@ -75,36 +79,69 @@ def _check_gguf():
         }
 
 
-def load_gguf(gguf_or_filepath) -> Dict[str, Weight]:
+def load_gguf(source: PathLike | gguf.GGUFReader) -> Dict[str, Weight]:
     _check_gguf()
     assert gguf is not None
+    GGUFReader = gguf.GGUFReader
+
     graph = Graph.current
-    reader = gguf_or_filepath
-    if not isinstance(reader, gguf.GGUFReader):
-        reader = gguf.GGUFReader(gguf_or_filepath)
-    ret = {}
-    for tensor in reader.tensors:
-        # Dims are reversed for some reason:
-        # https://github.com/ggerganov/llama.cpp/blob/master/gguf-py/gguf/gguf_reader.py#L277
-        # We have to un-reverse them here.
-        shape = list(reversed(tensor.shape.tolist()))
-        dtype = _GGML_TO_DTYPE.get(tensor.tensor_type, None)
-        quantization_encoding = _QUANTIZED_GGML_DTYPES.get(
-            tensor.tensor_type, None
-        )
-        if quantization_encoding is not None:
-            assert dtype is None
+    reader = source if isinstance(source, GGUFReader) else GGUFReader(source)
+    return {
+        tensor.name: parse_weight(tensor)(graph) for tensor in reader.tensors
+    }
+
+
+WeightReader = Callable[[Graph], Weight]
+
+
+def parse_weight(tensor: gguf.ReaderTensor) -> WeightReader:
+    _check_gguf()
+    assert gguf is not None
+    # Dims are reversed for some reason:
+    # https://github.com/ggerganov/llama.cpp/blob/master/gguf-py/gguf/gguf_reader.py#L277
+    # We have to un-reverse them here.
+    shape = list(reversed(tensor.shape.tolist()))
+    encoding = None
+    if (dtype := _GGML_TO_DTYPE.get(tensor.tensor_type)) is None:
+        if encoding := _QUANTIZED_GGML_DTYPES.get(tensor.tensor_type):
             # Quantized dtypes are treated as uint8 values.
             dtype = DType.uint8
             shape = gguf.quant_shape_to_byte_shape(shape, tensor.tensor_type)
-        if dtype is None:
+        else:
             raise ValueError(f"Unknown GGML DType: {tensor.tensor_type}.")
-        ret[tensor.name] = graph.add_weight(
-            tensor.name,
-            dtype,
-            shape,
-            tensor.data.filename,
-            tensor.data_offset,
-            quantization_encoding,
-        )
-    return ret
+
+    return lambda graph: graph.add_weight(
+        name=tensor.name,
+        dtype=dtype,
+        shape=shape,
+        filepath=tensor.data.filename,
+        offset=tensor.data_offset,
+        quantization_encoding=encoding,
+    )
+
+
+class Weights:
+    _reader: gguf.GGUFReader
+    _tensors: dict[str, gguf.ReaderTensor]
+    _prefix: str
+
+    def __init__(self, reader: gguf.GGUFReader, tensors=None, prefix: str = ""):
+        self._reader = reader
+        self._tensors = tensors or {t.name: t for t in reader.tensors}
+        self._prefix = prefix
+
+    def __iter__(self):
+        for name, tensor in self._tensors.items():
+            if name.startswith(self._prefix):
+                yield tensor
+
+    def __getattr__(self, attr) -> WeightReader | Weights:
+        full_path = f"{self._prefix}{attr}"
+        if tensor := self._tensors.get(full_path):
+            return parse_weight(tensor)
+        elif not any(name.startswith(full_path) for name in self._tensors):
+            raise AttributeError(f"No weight {full_path} found")
+        return Weights(self._reader, self._tensors, prefix=full_path + ".")
+
+    def __getitem__(self, idx: int) -> WeightReader | Weights:
+        return self.__getattr__(str(idx))
