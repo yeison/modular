8615cbb6c70d4f926bab9133b16ae6f6d129240a
akirchhoff-modular <akirchhoff@modular.com>
2024-09-05 12:12:58 -0700

    [EngineAPI] Add integration test for Python opaque usage (#46653)
    This does not currently pass for two reasons.
    
    1. This is part of a PR stack with other required changes, that are
    needed before this has any hope of passing.
    2. With the state of the other PRs at this time, there is a memory
    lifetime issue causing crashes (MSDK-912).
    
    However, it still shows the way this API is expected to work (possibly
    with the exception of `drop_counter` -- I feel like this shouldn't be
    needed, but we don't know yet; currently we don't even get
    `bump_counter` to run).
    
    Part of MSDK-843.
    
    MAX_GRAPH_API_ORIG_REV_ID: f65e8c0f67c1fb0821fba4d8760d5c04a1267f5e

---
+++ tests/integration/max/graph/test_opaque.py
@@ -0,0 +1,62 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+import os
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from max.engine import InferenceSession
+from max.graph import Graph, ops
+from max.graph.type import SymbolicDim, _OpaqueType as OpaqueType
+
+
+@pytest.fixture
+def counter_ops_path() -> Path:
+    return Path(os.environ["COUNTER_OPS_PATH"])
+
+
+@pytest.mark.skip(reason="MSDK-912: Memory lifetime issues cause this to crash")
+def test_opaque(counter_ops_path: Path) -> None:
+    session = InferenceSession()
+    counter_type = OpaqueType("Counter")
+
+    maker_graph = Graph("maker", input_types=[], output_types=[counter_type])
+    with maker_graph:
+        maker_graph.output(
+            ops.custom("make_counter", [], [counter_type.to_mlir()])[0]
+        )
+    maker_compiled = session.load(
+        maker_graph, custom_extensions=counter_ops_path
+    )
+
+    bumper_graph = Graph("bumper", input_types=[counter_type], output_types=[])
+    with bumper_graph:
+        ops.custom("bump_counter", [bumper_graph.inputs[0]], [])
+        bumper_graph.output()
+    bumper_compiled = session.load(
+        bumper_graph, custom_extensions=counter_ops_path
+    )
+
+    dropper_graph = Graph(
+        "dropper", input_types=[counter_type], output_types=[]
+    )
+    with dropper_graph:
+        ops.custom("drop_counter", [dropper_graph.inputs[0]], [])
+        dropper_graph.output()
+    dropper_compiled = session.load(
+        dropper_graph, custom_extensions=counter_ops_path
+    )
+
+    counter = maker_compiled.execute()["output0"]
+    for i in range(5):
+        bumper_compiled.execute(input0=counter)
+    dropper_compiled.execute(input0=counter)
+
+
+if __name__ == "__main__":
+    test_opaque()
