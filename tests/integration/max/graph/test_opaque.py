5a3a72bfe5aee64748bf9d8849f7311bdc00748c
akirchhoff-modular <akirchhoff@modular.com>
2024-09-13 14:14:50 -0700

    [EngineAPI] Use fixtures in opaque tests (#47187)
    More tests will need to use these graphs, so move them to fixtures to
    reduce repetition.
    
    I also removed the `__main__` support. This was a holdover from my
    initial work implementing this before I got it into Bazel -- now that
    it's in Bazel we can rely on always running through pytest.
    
    MAX_GRAPH_API_ORIG_REV_ID: a4c9a8e0d4244f4b87e7d9237d96ec0ef883a5e8

--- tests/integration/max/graph/test_opaque.py
+++ tests/integration/max/graph/test_opaque.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import pytest
 from max.driver import Tensor
 from max.dtype import DType
-from max.engine import InferenceSession
+from max.engine import InferenceSession, Model
 from max.graph import Graph, ops
 from max.graph.type import TensorType
 from max.graph.type import _OpaqueType as OpaqueType
@@ -21,16 +21,21 @@ def counter_ops_path() -> Path:
     return Path(os.environ["COUNTER_OPS_PATH"])
 
 
-def test_opaque(session: InferenceSession, counter_ops_path: Path) -> None:
+@pytest.fixture
+def maker_model(session: InferenceSession, counter_ops_path: Path) -> Model:
     counter_type = OpaqueType("Counter")
-
     maker_graph = Graph("maker", input_types=[], output_types=[counter_type])
     with maker_graph:
         maker_graph.output(ops.custom("make_counter", [], [counter_type])[0])
     maker_compiled = session.load(
         maker_graph, custom_extensions=counter_ops_path
     )
+    return maker_compiled
 
+
+@pytest.fixture
+def bumper_model(session: InferenceSession, counter_ops_path: Path) -> Model:
+    counter_type = OpaqueType("Counter")
     bumper_graph = Graph("bumper", input_types=[counter_type], output_types=[])
     with bumper_graph:
         # TODO(MSDK-950): Avoid DCE in the graph compiler and remove return value.
@@ -43,7 +48,12 @@ def test_opaque(session: InferenceSession, counter_ops_path: Path) -> None:
     bumper_compiled = session.load(
         bumper_graph, custom_extensions=counter_ops_path
     )
+    return bumper_compiled
 
+
+@pytest.fixture
+def reader_model(session: InferenceSession, counter_ops_path: Path) -> Model:
+    counter_type = OpaqueType("Counter")
     reader_graph = Graph("reader", input_types=[counter_type], output_types=[])
     with reader_graph:
         c = ops.custom(
@@ -55,11 +65,16 @@ def test_opaque(session: InferenceSession, counter_ops_path: Path) -> None:
     reader_compiled = session.load(
         reader_graph, custom_extensions=counter_ops_path
     )
+    return reader_compiled
 
-    counter = maker_compiled.execute()["output0"]
+
+def test_opaque(
+    maker_model: Model, bumper_model: Model, reader_model: Model
+) -> None:
+    counter = maker_model.execute()["output0"]
     for i in range(5):
-        bumper_compiled.execute(input0=counter)
-    result = reader_compiled.execute(input0=counter)["output0"]
+        bumper_model.execute(input0=counter)
+    result = reader_model.execute(input0=counter)["output0"]
 
     assert (result == [5, 15]).all()
 
@@ -92,10 +107,3 @@ def test_opaque_driver_constructor(
     init[1] = 37
     counter = maker_compiled.execute(init)
     assert counter and counter[0]
-
-
-if __name__ == "__main__":
-    sess = InferenceSession()
-    path = Path(os.environ["COUNTER_OPS_PATH"])
-    test_opaque(sess, path)
-    test_opaque_driver_constructor(sess, path)
