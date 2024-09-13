e6bb2301f4bfa99b560dc44feda6dbd18b732677
Fabian Tschopp <fabian.tschopp@modular.com>
2024-09-13 19:02:54 +0200

    [EngineAPI] Fix integration test for Python opaque (#47157)
    The test did not execute all graphs previously due to DCE.
    
    This PR disables the `dropper` graph and adds a result to `bump_counter`
    to avoid DCE.
    A `read_counter` op which outputs an NDBuffer is added to assert the
    result on the host side after execution.
    
    Follow-up issues:
    MSDK-949
    MSDK-950
    
    MAX_GRAPH_API_ORIG_REV_ID: a4f6e587e0582560d263d6af0d3d35718342cc0b

--- tests/integration/max/graph/test_opaque.py
+++ tests/integration/max/graph/test_opaque.py
@@ -8,12 +8,12 @@ import os
 from pathlib import Path
 
 import pytest
-
 from max.driver import Tensor
 from max.dtype import DType
 from max.engine import InferenceSession
 from max.graph import Graph, ops
-from max.graph.type import _OpaqueType as OpaqueType, TensorType
+from max.graph.type import TensorType
+from max.graph.type import _OpaqueType as OpaqueType
 
 
 @pytest.fixture
@@ -33,26 +33,48 @@ def test_opaque(session: InferenceSession, counter_ops_path: Path) -> None:
 
     bumper_graph = Graph("bumper", input_types=[counter_type], output_types=[])
     with bumper_graph:
-        ops.custom("bump_counter", [bumper_graph.inputs[0]], [])
-        bumper_graph.output()
+        # TODO(MSDK-950): Avoid DCE in the graph compiler and remove return value.
+        c = ops.custom(
+            "bump_counter",
+            [bumper_graph.inputs[0]],
+            [TensorType(DType.bool, [1])],
+        )
+        bumper_graph.output(c[0])
     bumper_compiled = session.load(
         bumper_graph, custom_extensions=counter_ops_path
+    )
+
+    reader_graph = Graph("reader", input_types=[counter_type], output_types=[])
+    with reader_graph:
+        c = ops.custom(
+            "read_counter",
+            [reader_graph.inputs[0]],
+            [TensorType(DType.int32, [2])],
+        )
+        reader_graph.output(c[0])
+    reader_compiled = session.load(
+        reader_graph, custom_extensions=counter_ops_path
     )
 
     dropper_graph = Graph(
         "dropper", input_types=[counter_type], output_types=[]
     )
     with dropper_graph:
-        ops.custom("drop_counter", [dropper_graph.inputs[0]], [])
-        dropper_graph.output()
-    dropper_compiled = session.load(
-        dropper_graph, custom_extensions=counter_ops_path
-    )
+        c = ops.custom(
+            "drop_counter", [dropper_graph.inputs[0]], [counter_type]
+        )
+        dropper_graph.output(c[0])
+    # TODO(MSDK-949): Fix and re-enable: error: 'mo.custom' op [MO_TO_MOGG] Owned arguments not supported for opaque types in the kernel drop_counter
+    # dropper_compiled = session.load(
+    #    dropper_graph, custom_extensions=counter_ops_path
+    # )
 
     counter = maker_compiled.execute()["output0"]
     for i in range(5):
         bumper_compiled.execute(input0=counter)
-    dropper_compiled.execute(input0=counter)
+    result = reader_compiled.execute(input0=counter)["output0"]
+
+    assert (result == [5, 15]).all()
 
 
 def test_opaque_driver_constructor(
