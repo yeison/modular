690188bfdec4083eeb38b568da8cd899b6711038
Brendan Duke <bduke@modular.com>
2024-09-13 15:52:06 -0400

    Add ContiguousKVCacheCollection type.
    MAX_GRAPH_API_ORIG_REV_ID: aaacfe5728d54da49563a0feec63a02db696da78

--- max/graph/__init__.py
+++ max/graph/__init__.py
@@ -5,9 +5,9 @@
 # ===----------------------------------------------------------------------=== #
 """APIs to build inference graphs for MAX."""
 
+from . import dtype_promotion
 from . import ops
 from .graph import Graph
-from .value import Value, TensorValue, ValueLike
-from .type import DimLike, TensorType, Type
+from .value import Value, TensorValue, ValueLike, _OpaqueValue as OpaqueValue
+from .type import DimLike, TensorType, Type, _OpaqueType as OpaqueType
 from .weight import Weight
-from . import dtype_promotion
---
+++ tests/integration/max/graph/test_kv_cache.py
@@ -0,0 +1,115 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+from llama3.kv_cache import ContiguousKVCacheCollectionType
+from llama3.kv_cache_params import KVCacheParams
+from max.driver import CPU, Tensor
+from max.dtype import DType
+from max.engine import InferenceSession
+from max.graph import ops, Graph, TensorType, TensorValue
+
+
+class KVCacheModel:
+    def __init__(self, kv_params: KVCacheParams) -> None:
+        self.kv_params = kv_params
+
+    def __call__(
+        self,
+        key_cache: TensorValue,
+        value_cache: TensorValue,
+        cache_lengths: TensorValue,
+        seq_ids: TensorValue,
+        num_layers: TensorValue,
+        batch_size: TensorValue,
+    ) -> ContiguousKVCacheCollectionType:
+        """Constructs an initial ContiguousKVCacheCollection for use downstream.
+        """
+        op_name = f"contiguous_kv_cache_collection_h{self.kv_params.n_kv_heads}_d{self.kv_params.head_dim}_{self.kv_params.layout}"
+        return ops.custom(
+            op_name,
+            values=[
+                key_cache,
+                value_cache,
+                cache_lengths,
+                seq_ids,
+                num_layers,
+                batch_size,
+            ],
+            out_types=[ContiguousKVCacheCollectionType],
+        )[0]
+
+
+def test_kv_cache(session: InferenceSession) -> None:
+    kv_params = KVCacheParams(
+        DType.float32, n_kv_heads=8, head_dim=128, device=CPU()
+    )
+    cache_type = TensorType(
+        kv_params.dtype,
+        (
+            "start_pos",
+            "n_layers",
+            "batch_size",
+            "n_kv_heads",
+            "head_dim",
+        ),
+    )
+    batch_size_param = 32
+    cache_lengths_type = TensorType(DType.int32, (batch_size_param,))
+    seq_ids_type = TensorType(DType.int32, ("seq_len",))
+    int_scalar_type = TensorType(DType.int32, (1,))
+
+    graph = Graph(
+        "kv_cache_collection",
+        KVCacheModel(kv_params),
+        input_types=[
+            cache_type,
+            cache_type,
+            cache_lengths_type,
+            seq_ids_type,
+            int_scalar_type,
+            int_scalar_type,
+        ],
+    )
+    model = session.load(graph)
+
+    max_seq_len = 5
+    num_layers_param = 32
+    k_cache = Tensor(
+        shape=(
+            num_layers_param,
+            batch_size_param,
+            kv_params.n_kv_heads,
+            max_seq_len,
+            kv_params.head_dim,
+        ),
+        dtype=kv_params.dtype,
+    )
+    v_cache = Tensor(shape=k_cache.shape, dtype=kv_params.dtype)
+    cache_lengths = Tensor(shape=(batch_size_param,), dtype=DType.int32)
+    for i in range(5):
+        cache_lengths[i] = 1
+    for i in range(6, cache_lengths.shape[0]):
+        cache_lengths[i] = 0
+    seq_ids = Tensor(shape=(1,), dtype=DType.int32)
+    seq_ids[0] = 0
+    num_layers = Tensor(shape=(1,), dtype=DType.int32)
+    num_layers[0] = 1
+    batch_size = Tensor(shape=(1,), dtype=DType.int32)
+    batch_size[0] = 1
+
+    kv_cache_collection = model.execute(
+        k_cache,
+        v_cache,
+        cache_lengths,
+        seq_ids,
+        num_layers,
+        batch_size,
+    )
+    assert kv_cache_collection and kv_cache_collection[0]
+
+
+if __name__ == "__main__":
+    test_kv_cache(InferenceSession())
