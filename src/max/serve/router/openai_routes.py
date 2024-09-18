327a39811a0d34be03bc6ce834c9a49a87f6405d
Gaz Iqbal <gaziqbal@gmail.com>
2024-09-18 15:27:41 -0700

    [Serving] Provide a /completions endpoint for benchmarking (#47428)
    vLLM benchmarking uses the legacy OpenAI /completions API (as opposed to
    /chat/completions)
    This PR adds this endpoint to our serving.
    
    I can successfully run VLLM benchmarks via the following
    `python benchmark_serving.py --backend openai --model
    meta-llama/Meta-Llama-3.1-8B-Instruct --dataset-path
    ShareGPT_V3_unfiltered_cleaned_split.json --endpoint /v1/completions
    --num-prompts 10`
    
    SERVE_ORIG_REV_ID: 77adcad4aeb0e50e3d63ffe099813e5d45901ffb

--- src/max/serve/router/openai_routes.py
+++ src/max/serve/router/openai_routes.py
@@ -7,7 +7,10 @@
 
 import logging
 from datetime import datetime
-from typing import Annotated
+from typing import Annotated, AsyncGenerator, Literal, Optional, cast, List
+from abc import ABC, abstractmethod
+
+from pydantic import BaseModel, Field
 
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import JSONResponse, Response
@@ -28,54 +31,48 @@ from max.serve.schemas.openai import (
     CreateChatCompletionRequest,
     CreateChatCompletionResponse,
     CreateChatCompletionStreamResponse,
+    Logprobs,
     Logprobs2,
+    CreateCompletionRequest,
+    CreateCompletionResponse,
+    Choice,
+    CompletionUsage,
 )
+
 
 router = APIRouter(prefix="/v1")
 logger = logging.getLogger(__name__)
 
 
-def openai_request_message_to_text(message: ChatCompletionRequestMessage):
-    """
-    The content property of each ChatCompletionRequestMessages can be a
-    string or be a list of partial messages.
-    """
-    if isinstance(message.root.content, str):
-        return message.root.content
-    if message.root.content is None:
-        return ""
-    return "".join(
-        [
-            part.root.text
-            for part in message.root.content
-            if isinstance(
-                part.root,
-                ChatCompletionRequestMessageContentPartText,
-            )
-        ]
-    )
-
-
-class OpenAITokenResponseGenerator:
+class OpenAIResponseGenerator(ABC):
     def __init__(
         self,
         pipeline: TokenGeneratorPipeline,
         request: TokenGeneratorRequest,
     ):
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.pipeline = pipeline
         self.request = request
 
+    @abstractmethod
+    async def stream() -> AsyncGenerator[str, None]:
+        pass
+
+    @abstractmethod
+    async def complete() -> str:
+        pass
+
+
+class OpenAIChatResponseGenerator(OpenAIResponseGenerator):
     async def stream(self):
         response_idx = 0
         async for token in self.pipeline.next_token(self.request):
-            logger.debug(
+            self.logger.debug(
                 "Streaming: %s, TOKEN: %d, %s",
                 self.request,
                 response_idx,
                 token,
             )
-            if token is None:
-                break
             # We support N = 1 at the moment and will generate a single choice.
             # The choice index is set to 0.
             # https://platform.openai.com/docs/api-reference/chat/object
@@ -143,8 +140,29 @@ class OpenAITokenResponseGenerator:
         return response
 
 
+def openai_get_content_from_message(message: ChatCompletionRequestMessage):
+    """
+    The content property of each ChatCompletionRequestMessages can be a
+    string or be a list of partial messages.
+    """
+    if isinstance(message.root.content, str):
+        return message.root.content
+    if message.root.content is None:
+        return ""
+    return "".join(
+        [
+            part.root.text
+            for part in message.root.content
+            if isinstance(
+                part.root,
+                ChatCompletionRequestMessageContentPartText,
+            )
+        ]
+    )
+
+
 @router.post("/chat/completions")
-async def openai_chat_completions(
+async def openai_create_chat_completion(
     request: Request,
     pipeline: Annotated[TokenGeneratorPipeline, Depends(token_pipeline)],
 ) -> Response:
@@ -153,9 +171,8 @@ async def openai_chat_completions(
     completion_request = CreateChatCompletionRequest.model_validate(
         request_json
     )
-
     logger.info(
-        f"Processing {request_id} -"
+        f"Processing {request.url.path}, {request_id} -"
         f" {'streaming' if completion_request.stream else ''} {completion_request.model} request."
     )
 
@@ -164,7 +181,7 @@ async def openai_chat_completions(
         messages = [
             {
                 "role": message.root.role,
-                "content": openai_request_message_to_text(message),
+                "content": openai_get_content_from_message(message),
             }
             for message in completion_request.messages
         ]
@@ -185,7 +202,137 @@ async def openai_chat_completions(
         prompt=request_prompt,
         max_new_tokens=completion_request.max_tokens,
     )
-    response_generator = OpenAITokenResponseGenerator(
+    response_generator = OpenAIChatResponseGenerator(
+        pipeline,
+        token_request,
+    )
+    if completion_request.stream:
+        return EventSourceResponse(response_generator.stream())
+
+    response = await response_generator.complete()
+    return JSONResponse(response.model_dump_json())
+
+
+"""
+Legacy OpenAI /completion endpoint.
+https://platform.openai.com/docs/api-reference/completions
+Public benchmarking such as vLLM use this endpoint.
+"""
+
+
+class CompletionResponseStreamChoice(BaseModel):
+    index: int
+    text: str
+    logprobs: Optional[Logprobs] = None
+    finish_reason: Optional[Literal["stop", "length", "content_filter"]] = None
+
+
+class CompletionStreamResponse(BaseModel):
+    id: str
+    created: int
+    model: str
+    choices: List[CompletionResponseStreamChoice]
+    object: Literal["text_completion"]
+    usage: Optional[CompletionUsage] = Field(default=None)
+
+
+class OpenAICompletionResponseGenerator(OpenAIResponseGenerator):
+    async def stream(self):
+        response_idx = 0
+        async for token in self.pipeline.next_token(self.request):
+            self.logger.debug(
+                "Streaming: %s, TOKEN: %d, %s",
+                self.request,
+                response_idx,
+                token,
+            )
+            # We support N = 1 at the moment and will generate a single choice.
+            # The choice index is set to 0.
+            # https://platform.openai.com/docs/api-reference/chat/object
+            choices = [
+                CompletionResponseStreamChoice(
+                    index=0,
+                    text=token,
+                )
+            ]
+            # Each chunk is expected to have the same id
+            # https://platform.openai.com/docs/api-reference/chat/streaming
+            response = CompletionStreamResponse(
+                id=self.request.id,
+                choices=choices,
+                created=int(datetime.now().timestamp()),
+                model="",
+                object="text_completion",
+            )
+            response_idx += 1
+            yield response.model_dump_json()
+
+        logger.debug(
+            "Completed: %s, %d tokens",
+            self.request,
+            response_idx,
+        )
+        yield "[DONE]"
+
+    async def complete(self) -> CreateCompletionResponse:
+        completed_tokens = await self.pipeline.all_tokens(self.request)
+
+        response_message = "".join(completed_tokens)
+        response_choices = [
+            Choice(
+                index=0,
+                text=response_message,
+                finish_reason="stop",
+                logprobs=Logprobs(),
+            )
+        ]
+        response = CreateCompletionResponse(
+            id=self.request.id,
+            choices=response_choices,
+            created=int(datetime.now().timestamp()),
+            model="",
+            object="text_completion",
+            system_fingerprint=None,
+        )
+        return response
+
+
+def openai_get_prompt_from_completion_request(
+    request: CreateCompletionRequest,
+) -> str:
+    if isinstance(request.prompt, str):
+        return request.prompt
+    elif isinstance(request.prompt, list):
+        if isinstance(request.prompt[0], str):
+            prompt = "".join(cast(list[str], request.prompt))
+            return prompt
+    # We can do this for functional equivalent if needed.
+    # However, the /completions API is already legacy.
+    raise ValueError("Prompts of types other than strings are not supported.")
+
+
+@router.post("/completions")
+async def openai_create_completion(
+    request: Request,
+    pipeline: Annotated[TokenGeneratorPipeline, Depends(token_pipeline)],
+) -> Response:
+    request_id = request.state.request_id
+    request_json = await request.json()
+    completion_request = CreateCompletionRequest.model_validate(request_json)
+    logger.info(
+        f"Processing {request.url.path}, {request_id} -"
+        f" {'streaming' if completion_request.stream else ''} {completion_request.model} request."
+    )
+
+    request_prompt = openai_get_prompt_from_completion_request(
+        completion_request
+    )
+    token_request = TokenGeneratorRequest(
+        id=request_id,
+        prompt=request_prompt,
+        max_new_tokens=completion_request.max_tokens,
+    )
+    response_generator = OpenAICompletionResponseGenerator(
         pipeline,
         token_request,
     )
