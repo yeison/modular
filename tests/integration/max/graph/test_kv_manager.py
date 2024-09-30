107a8e2b6d991aa8895d089c93cdc2e44f4578e7
kcaverly <kylebcaverly@gmail.com>
2024-09-30 16:00:55 -0500

    [Pipelines] Introduce Resource Leases in KV Manager
    MAX_GRAPH_API_ORIG_REV_ID: 6b6952ebe1d975682c6c4afa4b60b214b41d3a94

--- tests/integration/max/graph/test_kv_manager.py
+++ tests/integration/max/graph/test_kv_manager.py
@@ -4,11 +4,72 @@
 #
 # ===----------------------------------------------------------------------=== #
 
+import asyncio
 from max.driver import CPU
 from max.dtype import DType
 from max.engine import InferenceSession
 from nn.kv_cache import ContiguousKVCacheManager
 from nn.kv_cache_params import KVCacheParams
+
+
+async def _claim_fetch_and_reset(kv_manager):
+    # Claim one and evaluate the list returned
+    seq_ids = await kv_manager.claim(batch_size=1)
+    assert len(seq_ids) == 1
+
+    # Claim four and evaluate the list returned.
+    seq_ids_2 = await kv_manager.claim(batch_size=4)
+    assert len(seq_ids_2) == 4
+
+    # Assert that we are not claiming the same seq_ids twice.
+    assert len(set(seq_ids).intersection(set(seq_ids_2))) == 0
+
+    kv_collection = kv_manager.fetch(seq_ids + seq_ids_2)
+    assert kv_collection is not None
+
+    # Check that resetting the cache succeeds.
+    await kv_manager.reset_cache()
+
+    # Check that the update function operates as intended.
+    seq_ids = await kv_manager.claim(batch_size=3)
+    _ = kv_manager.fetch(seq_ids)
+
+    for i in range(3):
+        valid_lengths = {}
+        for seq_id in seq_ids:
+            valid_lengths[seq_id] = 1
+
+        kv_manager.step(valid_lengths)
+
+        for seq_id in seq_ids:
+            assert kv_manager.cache_lengths[seq_id] == 1 + i
+
+    # Reset the cache, claim 4 ids, and pass 3 to the fetch.
+    # This tests that the cache_lengths is appropriately pulled.
+    await kv_manager.reset_cache()
+
+    seq_ids = await kv_manager.claim(batch_size=4)
+    kv_collection = kv_manager.fetch(seq_ids[1:])
+
+    await kv_manager.reset_cache()
+
+
+async def _claim_and_release(kv_manager: ContiguousKVCacheManager):
+    # Claim 5 ids
+    seq_ids = await kv_manager.claim(batch_size=5)
+
+    outstanding = 11
+
+    # Introspect to ensure that we've only reserved 5
+    # Max Batch size is 16, therefore the remaining is 11
+    assert kv_manager.semaphore._value == outstanding
+
+    for seq_id in seq_ids:
+        await kv_manager.release(seq_id)
+        outstanding += 1
+        assert kv_manager.semaphore._value == outstanding
+
+    await kv_manager.reset_cache()
 
 
 def test_kv_manager(session: InferenceSession) -> None:
@@ -26,40 +87,6 @@ def test_kv_manager(session: InferenceSession) -> None:
         device=CPU(),
     )
 
-    # Claim one and evaluate the list returned.
-    seq_ids = kv_manager.claim(batch_size=1)
-    assert len(seq_ids) == 1
+    asyncio.run(_claim_fetch_and_reset(kv_manager))
 
-    # Claim four and evaluate the list returned.
-    seq_ids_2 = kv_manager.claim(batch_size=4)
-    assert len(seq_ids_2) == 4
-
-    # Assert that we are not claiming the same seq_ids twice.
-    assert len(set(seq_ids).intersection(set(seq_ids_2))) == 0
-
-    kv_collection = kv_manager.fetch(seq_ids + seq_ids_2)
-    assert kv_collection is not None
-
-    # Check that resetting the cache succeeds.
-    kv_manager.reset_cache()
-
-    # Check that the update function operates as intended.
-    seq_ids = kv_manager.claim(batch_size=3)
-    _ = kv_manager.fetch(seq_ids)
-
-    for i in range(3):
-        valid_lengths = {}
-        for seq_id in seq_ids:
-            valid_lengths[seq_id] = 1
-
-        kv_manager.step(valid_lengths)
-
-        for seq_id in seq_ids:
-            assert kv_manager.cache_lengths[seq_id] == 1 + i
-
-    # Reset the cache, claim 4 ids, and pass 3 to the fetch.
-    # This tests that the cache_lengths is appropriately pulled.
-    kv_manager.reset_cache()
-
-    seq_ids = kv_manager.claim(batch_size=4)
-    kv_collection = kv_manager.fetch(seq_ids[1:])
+    asyncio.run(_claim_and_release(kv_manager))
