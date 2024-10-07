a867bb349c13bed0ca834a5f7ba3f8710dcf3830
Rashid Kaleem <rashid@modular.com>
2024-10-07 11:58:58 -0500

    [Serving] Enhance evaluate_batch_scenarios (#48335)
    Adds echo-gen model to the batch scenario evaluator script and also
    refactors to enable plugging in new scenarios.
    
    Signed-off-by: Rashid Kaleem <rashid@modular.com>
    SERVE_ORIG_REV_ID: 8d8e5e755f24e173c0df3c7f5d637ef90354797b

--- src/max/serve/pipelines/echo_gen.py
+++ src/max/serve/pipelines/echo_gen.py
@@ -12,6 +12,8 @@ from typing import Optional
 class EchoTokenGeneratorContext:
     prompt: str
     index: int
+    max_tokens: int
+    tokens: str = ""
 
 
 @dataclass
@@ -19,19 +21,21 @@ class EchoTokenGenerator:
     async def new_context(
         self, prompt: str, max_new_tokens: Optional[int] = None
     ) -> EchoTokenGeneratorContext:
-        if max_new_tokens is not None:
-            raise NotImplementedError("max_new_tokens is not supported.")
-        return EchoTokenGeneratorContext(prompt, len(prompt))
+        return EchoTokenGeneratorContext(
+            prompt, 0, max_new_tokens if max_new_tokens else len(prompt)
+        )
 
     async def next_token(
         self, batch: dict[str, EchoTokenGeneratorContext]
     ) -> dict[str, str]:
         for _, ctx in batch.items():
-            ctx.index -= 1
+            ctx.index += 1
+            if ctx.index <= len(ctx.prompt) and ctx.index <= ctx.max_tokens:
+                ctx.tokens += ctx.prompt[-ctx.index]
         return {
-            rid: ctx.prompt[ctx.index]
+            rid: ctx.prompt[-ctx.index]
             for rid, ctx in batch.items()
-            if ctx.index >= 0
+            if ctx.index <= len(ctx.prompt) and ctx.index <= ctx.max_tokens
         }
 
     async def release(self, context: EchoTokenGeneratorContext):
