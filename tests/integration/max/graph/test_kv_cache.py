98575feab125ad25b73cea2b494b5b5a48c1fc1a
Austin Doolittle <austin@modular.com>
2024-10-15 10:26:45 -0400

    [KVCache][Pipelines] Fuse KVCacheCollection ctor into the main graph
    This fused the KVCacheCollection constructor into the main graph,
    removing the need to call a subgraph in order to construct the
    collection object. This is expected to provide a minor perf improvement
    in the generation loop.
    
    MAX_GRAPH_API_ORIG_REV_ID: 4ecdf7c8c61518c1dcf4f664dc85b951c083c050

--- tests/integration/max/graph/test_kv_cache.py
+++ tests/integration/max/graph/test_kv_cache.py
@@ -4,119 +4,151 @@
 #
 # ===----------------------------------------------------------------------=== #
 
+import pytest
 from max.driver import CPU, Tensor
 from max.dtype import DType
 from max.engine import InferenceSession
-from max.graph import Graph, TensorType, TensorValue, ops
+from max.graph import (
+    Graph,
+    TensorType,
+)
 
-from nn.kv_cache import KVCacheParams, ContiguousKVCacheCollectionType
-
-
-class KVCacheModel:
-    def __init__(self, kv_params: KVCacheParams) -> None:
-        self.kv_params = kv_params
-
-    def __call__(
-        self,
-        key_cache: TensorValue,
-        value_cache: TensorValue,
-        cache_lengths: TensorValue,
-        is_cache_empty: TensorValue,
-        seq_ids: TensorValue,
-        num_layers: TensorValue,
-        batch_size: TensorValue,
-    ) -> ContiguousKVCacheCollectionType:
-        """Constructs an initial ContiguousKVCacheCollection for use downstream.
-        """
-        op_name = f"contiguous_kv_cache_collection_h{self.kv_params.n_kv_heads}_d{self.kv_params.head_dim}_bshd"
-        return ops.custom(
-            op_name,
-            values=[
-                key_cache,
-                value_cache,
-                cache_lengths,
-                is_cache_empty,
-                seq_ids,
-                num_layers,
-                batch_size,
-            ],
-            out_types=[ContiguousKVCacheCollectionType()],
-        )[0]
+from nn.kv_cache import (
+    KVCacheParams,
+    FetchContiguousKVCacheCollection,
+    FetchContinuousBatchingKVCacheCollection,
+    KVCacheStrategy,
+    load_kv_manager,
+)
 
 
-def test_kv_cache(session: InferenceSession) -> None:
+@pytest.mark.asyncio
+async def test_kv_cache_length_contiguous() -> None:
+    """Tests that KV cache collections return the expected cache length."""
     kv_params = KVCacheParams(
-        DType.float32, n_kv_heads=8, head_dim=128, device=CPU()
+        dtype=DType.float32,
+        n_kv_heads=8,
+        head_dim=128,
+        device=CPU(),
+        cache_strategy=KVCacheStrategy.CONTIGUOUS,
     )
-    cache_type = TensorType(
-        kv_params.dtype,
-        (
-            "start_pos",
-            "n_layers",
-            "batch_size",
-            "n_kv_heads",
-            "head_dim",
-        ),
+    session = InferenceSession()
+    kv_manager = load_kv_manager(
+        params=kv_params,
+        max_cache_batch_size=1,
+        max_seq_len=512,
+        num_layers=32,
+        session=session,
+        device=CPU(),
     )
-    batch_size_param = 32
-    cache_lengths_type = TensorType(DType.int32, (batch_size_param,))
-    seq_ids_type = TensorType(DType.int32, ("seq_len",))
-    int_scalar_type = TensorType(DType.int32, (1,))
-    is_cache_empty_type = TensorType(DType.bool, (1,))
+
+    # Reserve a slot in the KV cache manager.
+    seq_id = await kv_manager.claim(n=1)
+    seq_id = seq_id[0]
+
+    # Set the cache lengths first by "stepping".
+    expected_cache_len = 42
+    kv_manager.step(valid_lengths={seq_id: expected_cache_len})
+
+    # Construct a KV cache collection with the given cache length.
+    kv_tuple = kv_manager.fetch(seq_ids=[seq_id])
+    assert len(kv_tuple) == 4
+
     graph = Graph(
-        "kv_cache_collection",
-        KVCacheModel(kv_params),
+        "create_collection",
+        FetchContiguousKVCacheCollection(kv_params),
         input_types=[
-            cache_type,
-            cache_type,
-            cache_lengths_type,
-            is_cache_empty_type,
-            seq_ids_type,
-            int_scalar_type,
-            int_scalar_type,
+            # key_cache
+            TensorType(
+                kv_params.dtype,
+                shape=[
+                    "num_layers",
+                    "batch_size",
+                    "max_seq_len",
+                    "num_kv_heads",
+                    "head_dim",
+                ],
+            ),
+            # value_cache
+            TensorType(
+                kv_params.dtype,
+                shape=[
+                    "num_layers",
+                    "batch_size",
+                    "max_seq_len",
+                    "num_kv_heads",
+                    "head_dim",
+                ],
+            ),
+            # cache_lengths
+            TensorType(DType.uint32, shape=["batch_size"]),
+            # is_cache_empty
+            TensorType(DType.bool, shape=[1]),
         ],
     )
-    model = session.load(graph)
 
-    max_seq_len = 5
-    num_layers_param = 32
-    k_cache = Tensor(
-        shape=(
-            num_layers_param,
-            batch_size_param,
-            kv_params.n_kv_heads,
-            max_seq_len,
-            kv_params.head_dim,
-        ),
-        dtype=kv_params.dtype,
+    outputs = session.load(graph).execute(*kv_tuple)
+    kv_collection = outputs[0]
+    assert kv_collection is not None
+
+
+@pytest.mark.asyncio
+async def test_kv_cache_length_continuous() -> None:
+    """Tests that KV cache collections return the expected cache length."""
+    kv_params = KVCacheParams(
+        dtype=DType.float32,
+        n_kv_heads=8,
+        head_dim=128,
+        device=CPU(),
+        cache_strategy=KVCacheStrategy.CONTINUOUS,
     )
-    v_cache = Tensor(shape=k_cache.shape, dtype=kv_params.dtype)
-    cache_lengths = Tensor(shape=(batch_size_param,), dtype=DType.int32)
-    for i in range(5):
-        cache_lengths[i] = 1
-    for i in range(6, cache_lengths.shape[0]):
-        cache_lengths[i] = 0
-
-    is_cache_empty = Tensor.zeros(shape=(1,), dtype=DType.bool)
-    is_cache_empty[0] = False
-    seq_ids = Tensor(shape=(1,), dtype=DType.int32)
-    seq_ids[0] = 0
-    num_layers = Tensor(shape=(1,), dtype=DType.int32)
-    num_layers[0] = 1
-    batch_size = Tensor(shape=(1,), dtype=DType.int32)
-    batch_size[0] = 1
-
-    kv_cache_collection = model.execute(
-        k_cache,
-        v_cache,
-        cache_lengths,
-        is_cache_empty,
-        seq_ids,
-        num_layers,
-        batch_size,
+    session = InferenceSession()
+    kv_manager = load_kv_manager(
+        params=kv_params,
+        max_cache_batch_size=1,
+        max_seq_len=512,
+        num_layers=32,
+        session=session,
+        device=CPU(),
     )
-    assert kv_cache_collection and kv_cache_collection[0]
 
+    # Reserve a slot in the KV cache manager.
+    seq_id = await kv_manager.claim(n=1)
+    seq_id = seq_id[0]
 
-if __name__ == "__main__":
-    test_kv_cache(InferenceSession())
+    # Set the cache lengths first by "stepping".
+    expected_cache_len = 42
+    kv_manager.step(valid_lengths={seq_id: expected_cache_len})
+
+    # Construct a KV cache collection with the given cache length.
+    kv_tuple = kv_manager.fetch(seq_ids=[seq_id])
+    assert len(kv_tuple) == 4
+
+    graph = Graph(
+        "create_collection",
+        FetchContinuousBatchingKVCacheCollection(kv_params),
+        input_types=[
+            # kv_blocks
+            TensorType(
+                kv_params.dtype,
+                shape=[
+                    "num_blocks",
+                    2,
+                    "num_layers",
+                    "max_seq_len",
+                    "num_kv_heads",
+                    "head_dim",
+                ],
+            ),
+            # cache_lengths
+            TensorType(DType.uint32, shape=["batch_size"]),
+            # lookup_table
+            TensorType(DType.uint32, shape=["batch_size"]),
+            # is_cache_empty
+            TensorType(DType.bool, shape=[1]),
+        ],
+    )
+
+    outputs = session.load(graph).execute(*kv_tuple)
+    kv_collection = outputs[0]
+    assert kv_collection is not None
