d550ea434ed426ce4a841dd4ceecaf4d07fd60b6
Joe Loser <joe@modular.com>
2024-10-17 17:00:35 -0600

    [stdlib] Enable pybind integration tests on Linux (#49355)
    Apply similar changes as done in
    
    [Internal link]
    to the two test files. Rework the invocation of the linker in the
    `mojo_pybind` script to work in the raw bindings case where we invoke
    the linker directly instead of using `mojo build`. More specifically,
    use `--whole-archive` instead of `-force_load` so this equivalent logic
    works on both Linux and macOS now. Previously, `-force_load` only worked
    on macOS.
    
    In the future, we may be able to rework that part of the script if `mojo
    build` is taught how to produce dynamic libraries generally from an
    existing static archive.
    
    Fixes MSTDL-894.
    
    PYTHON_BINDINGS_ORIG_REV_ID: d519907a3780c104373566fb206bc9eea4ca649c

--- mojo/integration-test/python-extension-modules/feature-overview/main.py
+++ mojo/integration-test/python-extension-modules/feature-overview/main.py
@@ -3,21 +3,14 @@
 # This file is Modular Inc proprietary.
 #
 # ===----------------------------------------------------------------------=== #
-# TODO(MSTDL-894): Support running this test on Linux
-# REQUIRES: system-darwin
 # RUN: python3 -m mojo-pybind.main --raw-bindings %S/mojo_module.mojo
 # RUN: python3 %s
 
 import sys
-import os
 import unittest
 
 # Put the current directory (containing .so) on the Python module lookup path.
 sys.path.insert(0, "")
-
-# Force the Mojo standard library to load the libpython for the current Python
-# process.
-os.environ["MOJO_PYTHON_LIBRARY"] = sys.executable
 
 # Imports from 'mojo_module.so'
 import mojo_module as feature_overview
