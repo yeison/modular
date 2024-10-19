802572482d0e2b135912b8ce33a79bfb6966e799
Kern Handa <kernhanda@users.noreply.github.com>
2024-10-18 22:43:34 -0700

    [SDK] Add ability to print static strings during graph execution (#49252)
    MAX_GRAPH_API_ORIG_REV_ID: 60aad63fdf1450c5a4a8ea45ef47f4e4cd53f929

--- max/graph/ops/debug.py
+++ max/graph/ops/debug.py
@@ -4,15 +4,18 @@
 #
 # ===----------------------------------------------------------------------=== #
 """Ops to help with debugging."""
+from __future__ import annotations
+
 from max.mlir.dialects import mo
 
 from ..graph import Graph
 from ..value import TensorValue
 
 
-def print(value: TensorValue, label: str = "debug_tensor"):
+def print(value: str | TensorValue, label: str = "debug_tensor"):
     in_chain = Graph.current._current_chain
-    output = Graph.current._add_op(
-        mo.debug_tensor_print, in_chain, value, label=label
-    )[0]
+
+    op = mo.debug_print if isinstance(value, str) else mo.debug_tensor_print
+
+    output = Graph.current._add_op(op, in_chain, value, label=label)[0]
     Graph.current._update_chain(output)
--- tests/max/graph/test_debug.py
+++ tests/max/graph/test_debug.py
@@ -7,15 +7,15 @@
 
 from hypothesis import given
 from hypothesis import strategies as st
-from max.graph import Graph, TensorType
+from max.graph import Graph, TensorType, ops
 
 # Instead of testing mlir string escaping, just limit the label to something reasonable.
 printable_ascii = st.characters(min_codepoint=ord(" "), max_codepoint=ord("~"))
 
 
 @given(input_type=..., label1=printable_ascii, label2=printable_ascii)
-def test_prints(input_type: TensorType, label1: str, label2: str):
-    with Graph("print", input_types=[input_type]) as graph:
+def test_tensor_prints(input_type: TensorType, label1: str, label2: str):
+    with Graph("print_tensors", input_types=[input_type]) as graph:
         out = graph.inputs[0]
         chain_0 = graph._current_chain
         out.print(label1)
@@ -27,6 +27,32 @@ def test_prints(input_type: TensorType, label1: str, label2: str):
 
         assert str(graph._mlir_op).count("mo.debug.tensor.print") == 2
         assert label1 in str(graph._mlir_op)
+        assert label2 in str(graph._mlir_op)
+
+        assert chain_0 != chain_1
+        assert chain_1 != chain_2
+
+
+@given(
+    msg1=printable_ascii,
+    label1=printable_ascii,
+    msg2=printable_ascii,
+    label2=printable_ascii,
+)
+def test_prints(msg1: str, label1: str, msg2: str, label2: str):
+    with Graph("print") as graph:
+        chain_0 = graph._current_chain
+        ops.print(msg1, label1)
+        chain_1 = graph._current_chain
+        ops.print(msg2, label2)
+        chain_2 = graph._current_chain
+
+        graph.output()
+
+        assert str(graph._mlir_op).count("mo.debug.print") == 2
+        assert msg1 in str(graph._mlir_op)
+        assert label1 in str(graph._mlir_op)
+        assert msg2 in str(graph._mlir_op)
         assert label2 in str(graph._mlir_op)
 
         assert chain_0 != chain_1
