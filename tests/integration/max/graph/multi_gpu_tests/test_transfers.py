25c78dec6bfcfc054255a2abf0ef0201d9db48d9
Kei-Ming Kwong <kming@users.noreply.github.com>
2024-11-16 12:58:18 -0500

    [Pipelines][Engine] Updates `api.py` to account for inputs and outputs device (#50851)
    This ensures that our python api accounts for the input and output
    device used by the inputs/outputs and ensures they are respected.
    
    Add E2E test for on-device IO
    
    Fixes AIPIPE-115
    
    MAX_GRAPH_API_ORIG_REV_ID: bad664b42d3ba50c6bebd8d510983a4a097acd36

--- tests/integration/max/graph/multi_gpu_tests/test_transfers.py
+++ tests/integration/max/graph/multi_gpu_tests/test_transfers.py
@@ -34,6 +34,27 @@ def create_multi_device_graph_with_cpu_io() -> Graph:
     return graph
 
 
+def create_multi_device_graph_with_gpu_io() -> Graph:
+    input_type0 = TensorType(
+        dtype=DType.float32,
+        shape=["batch", "channels"],
+        device=Device.CUDA(id=0),
+    )
+    input_type1 = TensorType(
+        dtype=DType.float32,
+        shape=["batch", "channels"],
+        device=Device.CUDA(id=1),
+    )
+    with Graph(
+        "add", input_types=(input_type0, input_type1, input_type0)
+    ) as graph:
+        gpu0_input1 = graph.inputs[1].to(Device.CUDA(0))
+        sum = ops.add(graph.inputs[0], graph.inputs[2])
+        sum2 = ops.add(sum, gpu0_input1)
+        graph.output(sum2)
+    return graph
+
+
 def test_cpu_io_graph_execution() -> None:
     """Tests multi-device transfers where inputs/outputs are on cpu."""
     graph = create_multi_device_graph_with_cpu_io()
@@ -59,3 +80,29 @@ def test_cpu_io_graph_execution() -> None:
     output = compiled.execute(a, b, c)
     # Check Executed Graph
     assert np.allclose((a_np + b_np + c_np), output[0].to_numpy())
+
+
+def test_gpu_io_graph_execution() -> None:
+    """Tests multi-device transfers where inputs/outputs are on cpu."""
+    graph = create_multi_device_graph_with_gpu_io()
+    # Check built graph
+    assert str(Device.CUDA(0)) in str(graph)
+    assert str(Device.CUDA(1)) in str(graph)
+    host = CPU()
+    device0 = CUDA(0)
+    device1 = CUDA(1)
+    session = InferenceSession(devices=[host, device0, device1])
+    compiled = session.load(graph)
+    # Check Compiled Graph
+    assert str(host) == str(compiled.devices[0])
+    assert str(device0) == str(compiled.devices[1])
+    assert str(device1) == str(compiled.devices[2])
+    a_np = np.ones((1, 1)).astype(np.float32)
+    b_np = np.ones((1, 1)).astype(np.float32)
+    c_np = np.ones((1, 1)).astype(np.float32)
+    a = Tensor.from_numpy(a_np).to(device0)
+    b = Tensor.from_numpy(b_np).to(device1)
+    c = Tensor.from_numpy(b_np).to(device0)
+    output = compiled.execute(a, b, c)
+    # Check Executed Graph
+    assert np.allclose((a_np + b_np + c_np), output[0].to(host).to_numpy())
