21c3425688cb357f0a201a496e7c3f26df24e822
Patrick Rachford <pat@modular.com>
2025-01-13 10:04:04 -0800

    [docs] Document the Custom APIs (#53703)
    Resolves
    [Internal link]
    
    ---------
    
    Co-authored-by: Brad Larson <bradlarson@modular.com>
    MAX_GRAPH_API_ORIG_REV_ID: c13f51f24abce43791a5ccfb549e3b37f0381409

--- max/graph/ops/custom.py
+++ max/graph/ops/custom.py
@@ -27,13 +27,13 @@ def custom(
     decorator.
 
     Args:
-        name: The op name provided to `max.register.op`.
+        name: The op name provided to ``max.register.op``.
         values: The op function's arguments.
         out_types: The list of op function's return type.
 
     Returns:
         Symbolic values representing the outputs of the op in the graph.
-        These correspond 1:1 with the types passed as `out_types`.
+        These correspond 1:1 with the types passed as ``out_types``.
     """
     graph = Graph.current
     symbol_attr = StringAttr.get(name, graph._context)
@@ -60,7 +60,7 @@ def inplace_custom(
     decorator.
 
     Args:
-        name: The op name provided to `max.register.op`.
+        name: The op name provided to ``max.register.op``.
         values: The op function's arguments.
     """
     # Unfortunately there's no existing way to mark a particular NDBuffer input
