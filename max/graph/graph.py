yeison/modular

commit afb6151a8516acdef51ab2bba8f42d095adbc5c9

[SDK] Support setting parameters in ops.custom (#53973)
Support setting extra parameters in ops.custom.

Testing:
- Updated test_opaque.py to use the new kernel extensibility API, and to
  use an opaque type representing a parameteric Mojo type.

This only allows setting int (IntegerAttr) and str (StringAttr)
Python types as parameters.
I want a slightly more involved test for DType, so will add that in a
follow up.

MAX_GRAPH_API_ORIG_REV_ID: 302c2c81b477585b2c7f4af6c54c9b5781aaec58

max/graph/ops/custom.py
@@ -9,7 +9,8 @@ from __future__ import annotations
 
 from typing import Iterable
 
-from max.mlir import StringAttr
+from max import mlir
+from max.mlir import IndexType, IntegerAttr, StringAttr
 from max.mlir.dialects import mo
 
 from ..graph import Graph
@@ -17,8 +18,24 @@ from ..type import Type, _ChainType
 from ..value import BufferValue, Value, _OpaqueValue
 
 
+def _parameter_attribute(
+    param: int | str, context: mlir.Context
+) -> mlir.Attribute:
+    """Converts a Python type to an MLIR attribute to parametrize a kernel."""
+    if isinstance(param, int):
+        return IntegerAttr.get(IndexType.get(context), param)
+    elif isinstance(param, str):
+        return StringAttr.get(param, context)
+    else:
+        msg = f"unsupported parameter type {type(param)} for custom op"
+        raise TypeError(msg)
+
+
 def custom(
-    name: str, values: list[Value], out_types: list[Type]
+    name: str,
+    values: list[Value],
+    out_types: list[Type],
+    parameters: dict[str, int | str] | None = None,
 ) -> list[Value]:
     """Creates a node to execute a custom graph operation in the graph.
 
@@ -30,6 +47,7 @@ def custom(
         name: The op name provided to ``max.register.op``.
         values: The op function's arguments.
         out_types: The list of op function's return type.
+        parameters: Dictionary of extra parameters expected by the kernel.
 
     Returns:
         Symbolic values representing the outputs of the op in the graph.
@@ -45,13 +63,24 @@ def custom(
         )
         raise TypeError(msg)
 
-    return graph._add_op(
+    results, custom_op = graph._add_op_get_op_with_results(
         mo.custom, [t.to_mlir() for t in out_types], values, symbol=symbol_attr
     )
 
+    if parameters is not None:
+        for name, param in parameters.items():
+            custom_op.attributes[name] = _parameter_attribute(
+                param, graph._context
+            )
+
+    return results
+
 
 def inplace_custom(
-    name: str, values: Iterable[Value], out_types: Iterable[Type] | None = None
+    name: str,
+    values: Iterable[Value],
+    out_types: Iterable[Type] | None = None,
+    parameters: dict[str, int | str] | None = None,
 ) -> list[Value]:
     """Creates a node to execute an in-place custom graph operation in the graph.
 
@@ -84,12 +113,18 @@ def inplace_custom(
     graph = Graph.current
     current_chain = graph._current_chain
 
-    *results, out_chain = graph._add_op(
+    (*results, out_chain), custom_op = graph._add_op_get_op_with_results(
         mo.custom,
         results_=[*out_mlir_types, _ChainType().to_mlir()],
         operands_=[*values, current_chain],
         symbol=StringAttr.get(name, graph._context),
     )
     graph._update_chain(out_chain._mlir_value)
+
+    if parameters is not None:
+        for name, param in parameters.items():
+            custom_op.attributes[name] = _parameter_attribute(
+                param, graph._context
+            )
 
     return results

tests/integration/max/graph/test_opaque.py
@@ -24,7 +24,14 @@ def maker_model(session: InferenceSession, counter_ops_path: Path) -> Model:
     counter_type = _OpaqueType("Counter")
     maker_graph = Graph("maker", input_types=[], output_types=[counter_type])
     with maker_graph:
-        maker_graph.output(ops.custom("make_counter", [], [counter_type])[0])
+        maker_graph.output(
+            ops.custom(
+                "make_counter",
+                values=[],
+                out_types=[counter_type],
+                parameters={"stride": 1},
+            )[0]
+        )
     maker_compiled = session.load(
         maker_graph, custom_extensions=counter_ops_path
     )
@@ -36,7 +43,12 @@ def bumper_model(session: InferenceSession, counter_ops_path: Path) -> Model:
     counter_type = _OpaqueType("Counter")
     bumper_graph = Graph("bumper", input_types=[counter_type], output_types=[])
     with bumper_graph:
-        ops.inplace_custom("bump_counter", [bumper_graph.inputs[0]], [])
+        ops.inplace_custom(
+            "bump_counter",
+            values=[bumper_graph.inputs[0]],
+            out_types=[],
+            parameters={"stride": 1},
+        )
         bumper_graph.output()
     bumper_compiled = session.load(
         bumper_graph, custom_extensions=counter_ops_path
@@ -51,8 +63,9 @@ def reader_model(session: InferenceSession, counter_ops_path: Path) -> Model:
     with reader_graph:
         c = ops.inplace_custom(
             "read_counter",
-            [reader_graph.inputs[0]],
-            [TensorType(DType.int32, [2])],
+            values=[reader_graph.inputs[0]],
+            out_types=[TensorType(DType.int32, [2])],
+            parameters={"stride": 1},
         )
         reader_graph.output(c[0])
     reader_compiled = session.load(
@@ -61,7 +74,7 @@ def reader_model(session: InferenceSession, counter_ops_path: Path) -> Model:
     return reader_compiled
 
 
-def test_opaque(
+def test_opaque_simple(
     maker_model: Model, bumper_model: Model, reader_model: Model
 ) -> None:
     counter = maker_model.execute_legacy()["output0"]
@@ -89,6 +102,7 @@ def test_opaque_driver_constructor(
                 "make_counter_from_tensor",
                 values=[maker_graph.inputs[0]],
                 out_types=[counter_type],
+                parameters={"stride": 1},
             )[0]
         )
     maker_compiled = session.load(

commit bfe6739459faa8a19e24d332d8ba67d03451e3f2

[SDK] Fix typo and missing docstring arg (#54051)
Fix a typo and missing docstring arg introduced in #53973.

MAX_GRAPH_API_ORIG_REV_ID: f89df9821e4cd50962dfb1e123f6fbb1a465b0f2

max/graph/graph.py
@@ -337,7 +337,7 @@ class Graph:
         new_params = dict.fromkeys(new_params.keys() - self._params.keys())
         self._params.update(new_params)
         if new_params:
-            # Add the output parms to the op we just created.
+            # Add the output params to the op we just created.
             param_decl = _graph.dim_param_decl_array_attr(
                 self._context,
                 [

max/graph/ops/custom.py
@@ -91,6 +91,7 @@ def inplace_custom(
     Args:
         name: The op name provided to ``max.register.op``.
         values: The op function's arguments.
+        parameters: Dictionary of extra parameters expected by the kernel.
     """
     # Unfortunately there's no existing way to mark a particular NDBuffer input
     # as needing to be backed by a `mo.buffer` value at the graph level.