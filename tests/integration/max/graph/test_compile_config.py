11044fae89e888404c576ec7f0a07775bb05877c
abdul dakkak <adakkak@modular.com>
2025-01-29 09:34:55 -0800

    [SDK] Add an example of how to use the mojo logger in a custom op (#54697)
    This shows how one can use the Mojo logger along with conditional
    enablement from the Graph API side. The idea here is that one can
    liberally put debug statements (with different levels) and turn all of
    them off at the Graph API level with zero overhead.
    
    MAX_GRAPH_API_ORIG_REV_ID: 364d9b9a5da9b6f2ac793a82c39ddca5202005a8

--- tests/integration/max/graph/test_compile_config.py
+++ tests/integration/max/graph/test_compile_config.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 import pytest
 from max.dtype import DType
-from max.engine import InferenceSession
+from max.engine import InferenceSession, LogLevel
 from max.graph import Graph, TensorType, ops
 
 
@@ -36,3 +36,23 @@ def test_compile_config_split_k_reduction_scheme(
     model = session.load(graph, custom_extensions=compile_config_ops_path)
     result = model.execute()[0].to_numpy()
     assert result == [2]
+
+
+def test_compile_config_use_logger(
+    capfd, session: InferenceSession, compile_config_ops_path: Path
+):
+    tensor_type = TensorType(dtype=DType.int32, shape=[1])
+    with Graph("graph", input_types=[]) as graph:
+        graph.output(ops.custom("use_logger", [], [tensor_type])[0])
+
+    session.set_mojo_log_level(LogLevel.DEBUG)
+    model = session.load(graph, custom_extensions=compile_config_ops_path)
+    result = model.execute()[0].to_numpy()
+
+    # On the Mojo side the logger level is set to DEBUG, so the result should be 10.
+    assert result == [10]
+
+    # On the Mojo side, the logger has printed "I'm a custom Mojo function!"
+    # We need to capture the output and check that it matches.
+    captured = capfd.readouterr()
+    assert captured.out == "ERROR::: I'm a custom Mojo function!\n"
