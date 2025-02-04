28b8d2881946bd1fbb7163f2a86d4120837f36d9
Katherine Wu <31663267+k-w-w@users.noreply.github.com>
2025-02-04 17:48:53 -0500

    [Graph] Add ops.gather test with valid and invalid inputs (#55172)
    There is a bug where if `op.gather(input, indices, axis)` is given
    indices that's greater than the input dimension, rubbish values are
    returned instead of raising an error.
    
    related to GEX-1808
    
    MAX_GRAPH_API_ORIG_REV_ID: 5da0b756b331044d4f617ae318d222724d4d65e2

---
+++ tests/integration/max/graph/test_gather.py
@@ -0,0 +1,48 @@
+# ===----------------------------------------------------------------------=== #
+#
+# This file is Modular Inc proprietary.
+#
+# ===----------------------------------------------------------------------=== #
+
+import numpy as np
+import torch
+from max.dtype import DType
+from max.graph import Graph, TensorType
+from max.graph.ops import gather
+
+
+def test_gather(session):
+    input_shape = [3, 2]
+    index_shape = [5, 1]
+    axis = 0
+    with Graph(
+        "conv2d",
+        input_types=[
+            TensorType(DType.int64, input_shape),
+            TensorType(DType.int64, index_shape),
+        ],
+    ) as graph:
+        input, index = graph.inputs
+
+        output = gather(input, index, axis=axis)
+        graph.output(output)
+
+    model = session.load(graph)
+
+    inputs = torch.Tensor([[0, 1], [2, 3], [4, 5]]).to(torch.int64)
+
+    # Test 1: Valid gather and indices.
+    index = torch.Tensor([[0], [1], [2], [1], [1]]).to(torch.int64)
+
+    actual = model(inputs, index)[0].to_numpy()
+    expected = torch.take_along_dim(inputs, index, dim=0).numpy()
+    np.testing.assert_equal(actual.reshape(5, 2), expected)
+
+    # Test 2: Invalid indices
+    # Should raise an error since indices must be between [0,3).
+    index = torch.Tensor([[0], [1], [2], [3], [4]]).to(torch.int64)
+
+    # TODO(GEX-1808): Uncomment the following line when calling the model raises
+    # an error.
+    # with pytest.raises(RuntimeError):
+    actual = model(inputs, index)[0].to_numpy()
