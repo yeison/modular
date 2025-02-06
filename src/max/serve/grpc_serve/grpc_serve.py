adf572147cd581777d22f1c5ba5c6de798b69a88
Zheng Hao Tan <zheng@modular.com>
2025-02-05 17:31:27 -0800

    [Pipelines] Move cli to max/entrypoints Move `cli` to `max/entrypoints`
    SERVE_ORIG_REV_ID: 20e7444dc91e23fe9bc518a5d9c3bec9fd6be482

--- src/max/serve/grpc_serve/grpc_serve.py
+++ src/max/serve/grpc_serve/grpc_serve.py
@@ -15,8 +15,8 @@ from typing import Callable
 import grpc
 import max.serve.grpc_serve.grpc_predict_v2_pb2 as pb2
 from grpc_reflection.v1alpha import reflection
+from max.entrypoints.cli import TextGenerationMetrics
 from max.pipelines import PipelineConfig, PipelineTokenizer, TextTokenizer
-from max.pipelines.cli import TextGenerationMetrics
 from max.pipelines.interfaces import TokenGenerator, TokenGeneratorRequest
 from max.serve.grpc_serve.grpc_predict_v2_pb2_grpc import (
     GRPCInferenceServiceServicer,
