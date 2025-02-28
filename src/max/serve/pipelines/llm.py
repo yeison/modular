d59b902ca746ac190cd6ccfe3962023620e55ecc
Zheng Hao Tan <zheng@modular.com>
2025-02-28 08:17:18 -0800

    [Pipelines] Decompose PipelineConfig into individual configs like SamplingConfig, ProfilingConfig etc. This is PR 1 of N in attempting to refactor our `PipelineConfig` into smaller configs that can better handle defaults vs. overrides from various entrypoints, be it CLI, env variables or (future) config files.
    SERVE_ORIG_REV_ID: 2d46467bc8974ced209ef16bd843742e2f07c67e

--- src/max/serve/pipelines/llm.py
+++ src/max/serve/pipelines/llm.py
@@ -430,7 +430,10 @@ def batch_config_from_pipeline_config(
 
     target_ce_batch_tokens = get_target_ce_batch_tokens(pipeline_config)
     assert pipeline_config.max_ce_batch_size is not None
-    if pipeline_config.cache_strategy == KVCacheStrategy.CONTINUOUS:
+    if (
+        pipeline_config.kv_cache_config.cache_strategy
+        == KVCacheStrategy.CONTINUOUS
+    ):
         batch_config = TokenGeneratorPipelineConfig.continuous_heterogenous(
             tg_batch_size=pipeline_config.max_batch_size,
             ce_batch_size=min(
@@ -443,13 +446,17 @@ def batch_config_from_pipeline_config(
             enable_chunked_prefill=pipeline_config.enable_chunked_prefill,
             enable_in_flight_batching=pipeline_config.enable_in_flight_batching,
         )
-    elif pipeline_config.cache_strategy == KVCacheStrategy.NAIVE:
+    elif (
+        pipeline_config.kv_cache_config.cache_strategy == KVCacheStrategy.NAIVE
+    ):
         batch_config = TokenGeneratorPipelineConfig.dynamic_homogenous(
             batch_size=pipeline_config.max_batch_size,
             batch_timeout=batch_timeout,
             max_forward_steps=pipeline_config.max_num_steps,
         )
-    elif pipeline_config.cache_strategy == KVCacheStrategy.PAGED:
+    elif (
+        pipeline_config.kv_cache_config.cache_strategy == KVCacheStrategy.PAGED
+    ):
         batch_config = TokenGeneratorPipelineConfig.paged(
             tg_batch_size=pipeline_config.max_batch_size,
             ce_batch_size=min(
@@ -464,12 +471,14 @@ def batch_config_from_pipeline_config(
         )
     else:
         raise ValueError(
-            f"{pipeline_config.cache_strategy} caching strategy is not"
+            f"{pipeline_config.kv_cache_config.cache_strategy} caching strategy is not"
             " supported by Serving."
         )
 
     log_str = "Server configured with:\n"
-    log_str += f"\tCache Strategy: {pipeline_config.cache_strategy}\n"
+    log_str += (
+        f"\tCache Strategy: {pipeline_config.kv_cache_config.cache_strategy}\n"
+    )
     log_str += f"\tBatch Size: {pipeline_config.max_batch_size}\n"
     log_str += f"\tChunked Prefill: {'Enabled' if pipeline_config.enable_chunked_prefill else 'Disabled'}\n"
     if pipeline_config.enable_chunked_prefill:
