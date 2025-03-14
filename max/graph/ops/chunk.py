13730c85758a31d3f62e52c434aeb9b35bb4c099
Brendan Hansknecht <bhansconnect@modular.com>
2025-03-13 18:02:02 -0700

    [graph-api] Make chunk op support algebraic dims (#57655)
    [graph-api] Make chunk op support algebraic dims
    
    This makes chunk more usable in our various graphs.
    That said, it requires splitting into chunks all of the same size.
    Once we have control flow in the graph, we should be able to make it
    flexible again.
    
    MAX_GRAPH_API_ORIG_REV_ID: ecf7effeaf2af4358d2ad3babefb27d44fdd1096

--- max/graph/ops/chunk.py
+++ max/graph/ops/chunk.py
@@ -5,17 +5,16 @@
 # ===----------------------------------------------------------------------=== #
 """Op implementation for chunk."""
 
-import math
 from typing import MutableSequence
 
-from ..type import _is_static_shape
+from ..type import Shape
 from ..value import TensorValue
 from .slice_tensor import SliceIndex, slice_tensor
 
 
 def chunk(a: TensorValue, chunks: int, dim: int = 0) -> list[TensorValue]:
     """
-    Chunk the tensor into chunks along the specified dim.
+    Chunk the tensor into an exact number of chunks along the specified dim.
 
     Args:
         a: The tensor to chunk.
@@ -30,40 +29,52 @@ def chunk(a: TensorValue, chunks: int, dim: int = 0) -> list[TensorValue]:
         >>> chunk(a, 2, 0)
         [TensorValue([1, 2]), TensorValue([3, 4])]
     """
+    # TODO(GEX-1943): once we have control flow in the graph, this can be updated to
+    # dynamic chunk counts while still supporting algebraic dims. For now,
+    # this will generate exactly chunks or fail.
 
-    if not _is_static_shape(a.shape):
-        raise ValueError("the chunk operation only supports static shapes")
-
-    if a.rank == 0:
-        return [a]
+    if a.rank == 0 and chunks > 1:
+        msg = f"Cannot split 1 value into {chunks=}"
+        raise ValueError(msg)
 
     # Check if the dimension is out of bounds. We put the error message
     # construction here to make sure we have the correct dim before we start
     # normalizing it.
-    err_msg = (
-        f"the dimension '{dim}' is out of bounds for tensor of rank {a.rank}"
-    )
+    dim_err_msg = f"'{dim=}' is out of bounds for tensor of rank {a.rank}"
+    chunks_err_msg = f"chunk: '{dim=}' of {a.shape=} must be exactly divisible into {chunks=}"
 
     if dim < 0:
         dim = a.rank + dim
 
     if a.rank < dim:
-        raise ValueError(err_msg)
+        raise ValueError(dim_err_msg)
 
-    n = int(a.shape[dim])
+    n = a.shape[dim]
 
     # Determine chunk size using ceiling division.
-    chunk_size = int(math.ceil(n / chunks))
+    chunk_size = (n + chunks - 1) // chunks
+    full_size = chunk_size * chunks
 
+    # Ensure exact divisbility
+    target_shape = Shape(a.shape)
+    target_shape[dim] = full_size
+    # The rebind adds a better compile and runtime time error message if hit.
+    a.rebind(target_shape, chunks_err_msg)
+    try:
+        # The reshape catches errors with static shapes right at build time.
+        a = a.reshape(target_shape)
+    except ValueError as e:
+        # This intentially does not reraise the initial error.
+        # The initial error is just noise used to figure out if we divide exactly.
+        raise ValueError(chunks_err_msg)
+
+    # Build a tuple of slice objects for all dimensions.
+    slices: MutableSequence[SliceIndex] = [slice(None)] * a.rank
     result = []
     for i in range(chunks):
         start = i * chunk_size
-        if start >= n:
-            break
-        end = min(start + chunk_size, n)
+        end = start + chunk_size
 
-        # Build a tuple of slice objects for all dimensions.
-        slices: MutableSequence[SliceIndex] = [slice(None)] * a.rank
         slices[dim] = slice(start, end)
         result.append(slice_tensor(a, slices))
     return result
--- tests/max/graph/ops/test_chunk.py
+++ tests/max/graph/ops/test_chunk.py
@@ -5,8 +5,7 @@
 # ===----------------------------------------------------------------------=== #
 """Test the max.graph Python bindings."""
 
-import math
-
+import pytest
 from conftest import axes, shapes, tensor_types
 from hypothesis import assume, given
 from hypothesis import strategies as st
@@ -20,15 +19,25 @@ chunks = st.integers(min_value=1, max_value=4)
 def test_chunk(input_type: TensorType, chunks: int, dim: int):
     assume(input_type.rank > 0)
     with Graph("chunk", input_types=[input_type]) as graph:
-        outs = ops.chunk(graph.inputs[0], chunks, dim=dim)
         target_shape = input_type.shape.static_dims
         n = target_shape[dim]
-        chunk_size = int(math.ceil(n / chunks))
+        assume(int(n) % chunks == 0)
+        outs = ops.chunk(graph.inputs[0], chunks, dim=dim)
+        chunk_size = (n + chunks - 1) // chunks
         for i in range(chunks):
             start = i * chunk_size
-            if start >= n:
-                break
             end = min(start + chunk_size, n)
             expected_length = end - start
             assert int(outs[i].shape[dim]) == expected_length
         graph.output(outs[0])
+
+
+@given(input_type=shared_types, chunks=chunks, dim=axes(shared_types))
+def test_chunk_not_exact(input_type: TensorType, chunks: int, dim: int):
+    assume(input_type.rank > 0)
+    with Graph("chunk", input_types=[input_type]) as graph:
+        target_shape = input_type.shape.static_dims
+        n = target_shape[dim]
+        assume(int(n) % chunks != 0)
+        with pytest.raises(ValueError, match="must be exactly divisible"):
+            outs = ops.chunk(graph.inputs[0], chunks, dim=dim)
