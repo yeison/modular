9c8ff3380e5e2fb65e7c73ae3d93034b230e2b9b
Brendan Duke <bduke@modular.com>
2025-03-26 21:07:43 -0400

    [SDK] Remove a workaround faking max_lengths on GPU
    Remove a workaround where we pretended `max_lengths` was on GPU, since
    the underlying device assignment issue was fixed.
    
    MODULAR_ORIG_COMMIT_REV_ID: 29dc7dd25330032840a5972b5d13e78c3a42c8de

--- src/max/pipelines/kv_cache/continuous_batching_cache.py
+++ src/max/pipelines/kv_cache/continuous_batching_cache.py
@@ -393,14 +393,7 @@ class ContinuousBatchingKVCacheManager(KVCacheManager):
                 max_lengths=TensorType(
                     DType.uint32,
                     shape=["steps_remaining", 2],
-                    # TODO: This is a hack introduced to remediate a negative side effect
-                    # of the graph compiler changes introduced in #52793.
-                    # We are tricking the compiler into thinking that the max_lengths
-                    # tensor is on the device, even though it is on the host.
-                    # With #52793 graph compiler changes, the compiler would
-                    # insert erroneous device transfers that lead to CUDA_ILLEGAL_ADDRESS
-                    # errors if we don't do this forceful device specification.
-                    device=DeviceRef(self.devices[i].label, self.devices[i].id),
+                    device=DeviceRef.CPU(),
                 ),
             )
             for i in range(len(self.devices))
