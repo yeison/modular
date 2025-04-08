a217c0783a1492f19e79c2b50ce57e03945a8d1c
Kate Caldwell <85645843+katelyncaldwell@users.noreply.github.com>
2025-04-07 19:46:12 -0700

    [Pipelines] Add Mixture of Experts layer for Llama4.  (#59236)
    MODULAR_ORIG_COMMIT_REV_ID: 9ca3aee9e0523c9f67b2e5a1cb79f0b63c3a8e62

--- src/max/pipelines/architectures/llama4/mix_of_experts.py
+++ src/max/pipelines/architectures/llama4/mix_of_experts.py
@@ -14,7 +14,7 @@
 """Mixture of Experts Layer."""
 
 from max.dtype import DType
-from max.graph import TensorValue, Weight
+from max.graph import TensorValue, Weight, ops
 from max.nn.layer import Module
 from max.nn.linear import LinearV2
 
@@ -29,6 +29,12 @@ class MoE(Module):
     num_experts: int
     """Total number of experts."""
 
+    intermediate_size: int
+    """Hidden dimension size for MoE intermediate layer."""
+
+    intermediate_size_mlp: int
+    """Hidden dimension size for MoE experts mlp layer."""
+
     dtype: DType
     """Data type for weights."""
 
@@ -37,31 +43,34 @@ class MoE(Module):
         hidden_dim: int = 5120,
         top_k: int = 1,
         num_experts: int = 16,
+        intermediate_size: int = 8192,
+        intermediate_size_mlp: int = 16384,
         dtype: DType = DType.bfloat16,
     ):
         """
         Args:
-            num_experts_per_tok: Number of experts to route each token to.
-            ep_size: Size of expert parallel group.
-            experts_per_rank: Number of experts per rank.
-            moe_intermediate_size: Hidden dimension size for MoE intermediate layer.
-            max_position_embeddings: Maximum sequence length.
+            hidden_dim: Hidden dimension size.
+            top_k: Number of experts to route each token to.
+            num_experts: Total number of experts.
+            intermediate_size: Hidden dimension size for MoE intermediate layer.
+            intermediate_size_mlp: Hidden dimension size for MoE MLP layer.
+            dtype: Data type for weights.
         """
         super().__init__()
 
         self.top_k = top_k
         self.hidden_dim = hidden_dim
         self.num_experts = num_experts
-
-        # TODO: Weight shapes are hardcoded for now. Will be updated to use parameters from config.
+        self.intermediate_size = intermediate_size
+        self.intermediate_size_mlp = intermediate_size_mlp
 
         # Routed experts weights
         self.down_proj = Weight(
             name="experts.down_proj.weight",
             shape=(
-                16,
-                8192,
-                5120,
+                self.num_experts,
+                self.intermediate_size,
+                self.hidden_dim,
             ),
             dtype=dtype,
         )
@@ -69,27 +78,27 @@ class MoE(Module):
         self.gate_up_proj = Weight(
             name="experts.gate_up_proj.weight",
             shape=(
-                16,
-                5120,
-                16384,
+                self.num_experts,
+                self.hidden_dim,
+                self.intermediate_size_mlp,
             ),
             dtype=dtype,
         )
 
         # Shared experts weights
         self.shared_expert_up_proj = LinearV2(
-            in_dim=5120,
-            out_dim=8192,
+            in_dim=self.hidden_dim,
+            out_dim=self.intermediate_size,
             dtype=dtype,
         )
         self.shared_expert_down_proj = LinearV2(
-            in_dim=8192,
-            out_dim=5120,
+            in_dim=self.intermediate_size,
+            out_dim=self.hidden_dim,
             dtype=dtype,
         )
         self.shared_expert_gate_proj = LinearV2(
-            in_dim=5120,
-            out_dim=8192,
+            in_dim=self.hidden_dim,
+            out_dim=self.intermediate_size,
             dtype=dtype,
         )
 
@@ -100,6 +109,37 @@ class MoE(Module):
         )
 
     def __call__(self, hidden_states: TensorValue):
-        raise NotImplementedError("Llama4 MoE layer not yet implemented")
+        hidden_states = ops.reshape(hidden_states, (-1, self.hidden_dim))
+        router_logits = self.router(hidden_states)
+        # (batch * seq_len, num_experts)
+        top_idx = ops.squeeze(ops.argmax(router_logits, axis=-1), axis=1)
+        # (batch * seq_len,)
+        router_probs = ops.sigmoid(ops.max(router_logits, axis=-1))
+        # (batch * seq_len, 1)
 
-        return None
+        top_down_proj = ops.gather(self.down_proj, top_idx, axis=0)
+        top_gate_up_proj = ops.gather(self.gate_up_proj, top_idx, axis=0)
+        # (batch * seq_len, h, w)
+
+        gate_up_projs = ops.unsqueeze(hidden_states, axis=1) @ top_gate_up_proj
+        # (batch * seq_len, 1, hidden_dim) @ (batch*seq_len, hidden_dim, intermediate_size) -> (batch*seq_len, 1, intermediate_size)
+
+        gate_up_projs = (
+            ops.silu(
+                gate_up_projs[:, :, : self.intermediate_size].cast(
+                    DType.float32
+                )
+            ).cast(DType.bfloat16)
+            * gate_up_projs[:, :, self.intermediate_size :]
+        )
+
+        down_projs = ops.squeeze(gate_up_projs @ top_down_proj, axis=1)
+        # (batch * seq_len, 16, 8192) @ (batch*seq_len, intermediate_size, 1) -> (batch * seq_len, hidden_dim)
+
+        shared_expert_out = self.shared_expert_down_proj(
+            ops.silu(
+                self.shared_expert_gate_proj(hidden_states).cast(DType.float32)
+            ).cast(DType.bfloat16)
+            * self.shared_expert_up_proj(hidden_states)
+        )
+        return router_probs * down_projs + shared_expert_out
