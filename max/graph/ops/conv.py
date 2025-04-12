70d2bf85a8527a6e360a4a085aa0fcc6e9ab0a51
Brendan Hansknecht <bhansconnect@modular.com>
2025-04-12 00:45:56 +0000

    Revert "[SDK] Use rmo.conv in conv2d and conv3d" (#59550)
    Breaks shape inference for pixtral: `br
    //SDK/integration-test/pipelines/python:verify_pipelines -- --pipeline
    pixtral-bfloat16 --devices gpu`
    
    returns:
    ```
    ValueError: Failed to create op 'conv':
    Inputs:
        input = TensorValue(dtype=bfloat16, shape=[Dim(1), Dim('image_height'), Dim('image_width'), Dim('num_channels')], device=None)
        filter = TensorValue(dtype=bfloat16, shape=[Dim(16), Dim(16), Dim(3), Dim(1024)], device=None)
        strides = Attribute(#mosh<ape[16, 16]> : !mosh.ape)
        dilations = Attribute(#mosh<ape[1, 1]> : !mosh.ape)
        paddings = Attribute(#mosh<ape[0, 0, 0, 0]> : !mosh.ape)
        num_groups = 1
    
    Diagnostics:
        [convolution] requires (input_channels == num_groups * filter_channels)
    Operation creation failed
    ```
    
    Reverts [Internal link]
    
    MAX_GRAPH_API_ORIG_REV_ID: cb837a350a8145907bfbc454582dfbe635fc4b87

--- max/graph/ops/conv.py
+++ max/graph/ops/conv.py
@@ -7,12 +7,54 @@
 
 from typing import Optional
 
+import numpy as np
+from max.dtype import DType
 from max.mlir.dialects import rmo
 
 from .. import dtype_promotion
 from ..graph import Graph
-from ..type import Shape
-from ..value import TensorValue, TensorValueLike
+from ..type import Dim, Shape
+from ..value import TensorType, TensorValue, TensorValueLike
+from .constant import constant
+
+
+def _calc_output_dimensions(
+    input_shape: Shape,
+    filter_shape: tuple[Dim, Dim],
+    padding: tuple[int, int, int, int],
+    stride: tuple[int, int],
+    dilation: tuple[int, int],
+) -> Shape:
+    """Computes the final dimensions of a rank-4 tensor after a 2-D convolution.
+
+    When a 2-D filter is applied to a tensor, the final height and width
+    dimensions are inset by the size of the filter, minus one, and then grown
+    by the padding applied before and after each dimension.
+
+    Args:
+        g: The Graph instance.
+        input: The shape of the rank-4 input tensor.
+        filter_shape: The shape of the filter being applied.
+        padding: The padding to apply to the height and width of the input
+                 tensor. Values are provided in (before_height, after_height,
+                 before_width, after_width) ordering.
+        stride: The stride of the filter, in (height, width) ordering.
+
+    Returns:
+        The resized shape of the tensor.
+    """
+    output_shape = input_shape  # output_shape[0] = input_shape[0] = batch_size
+
+    input_height, filter_height = input_shape[1], filter_shape[0]
+    output_shape[1] = (
+        input_height - filter_height + padding[0] + padding[1] + stride[0]
+    ) // stride[0]
+
+    input_width, filter_width = input_shape[2], filter_shape[1]
+    output_shape[2] = (
+        input_width - filter_width + padding[2] + padding[3] + stride[1]
+    ) // stride[1]
+    return output_shape
 
 
 def conv2d(
@@ -73,13 +115,6 @@ def conv2d(
         A symbolic tensor value with the convolution applied.
     """
     x, filter = dtype_promotion._promote_weak_dtypes(x, filter)
-
-    if x.dtype != filter.dtype:
-        raise ValueError(
-            "input and filter must resolve to the same strong dtype. input is"
-            f" {x.dtype}. filter is {filter.dtype}."
-        )
-
     if bias is not None:
         x, bias = dtype_promotion._promote_weak_dtypes(x, bias)
         if x.dtype != bias.dtype:
@@ -87,32 +122,60 @@ def conv2d(
                 "input and bias must resolve to the same strong dtype. input is"
                 f" {x.dtype}. bias is {bias.dtype}."
             )
-
         if bias.rank != 1:
             raise ValueError(
                 "bias for a 2-D convolution must be rank 1 with shape (out_channels,)"
             )
-
+    if x.dtype != filter.dtype:
+        raise ValueError(
+            "input and filter must resolve to the same strong dtype. input is"
+            f" {x.dtype}. filter is {filter.dtype}."
+        )
     if x.rank != 4:
         raise ValueError(
             "input to a 2-D convolution must be rank 4 with shape (batch_size,"
             " height, width, in_channels)"
         )
-
     if filter.rank != 4:
         raise ValueError(
             "filter for a 2-D convolution must be rank 4 with shape (height,"
             " width, in_channels / num_groups, out_channels)"
         )
+    if dilation != (1, 1):
+        raise ValueError("Non-unit dilation is not implemented yet!")
+
+    stride_constant = constant(np.array([stride[0], stride[1]]), DType.int64)
+
+    dilation_constant = constant(
+        np.array([dilation[0], dilation[1]]), DType.int64
+    )
+
+    padding_constant = constant(
+        np.array([padding[0], padding[1], padding[2], padding[3]]),
+        DType.int64,
+    )
+
+    groups_constant = constant(groups, DType.int64)
+
+    # TODO(MSDK-1196): create rmo.conv and move this to rmo
+    output_shape = _calc_output_dimensions(
+        x.shape,
+        filter_shape=(filter.shape[0], filter.shape[1]),
+        padding=padding,
+        stride=stride,
+        dilation=dilation,
+    )
+    output_shape[3] = filter.shape[3]  # out_channels
 
     conv_output = Graph.current._add_op(
-        rmo.conv,
-        TensorValue(x),
-        TensorValue(filter),
-        Shape(stride).to_mlir(),
-        Shape(dilation).to_mlir(),
-        Shape(padding).to_mlir(),
-        groups,
+        rmo.mo_conv,
+        TensorType(x.dtype, output_shape, x.device).to_mlir(),
+        x,
+        filter,
+        stride_constant,
+        dilation_constant,
+        padding_constant,
+        groups_constant,
     )[0].tensor
 
     if bias is not None:
@@ -177,13 +240,6 @@ def conv3d(
         Output shape = (batch_size, depth, height, width, out_channels).
     """
     x, filter = dtype_promotion._promote_weak_dtypes(x, filter)
-
-    if x.dtype != filter.dtype:
-        raise ValueError(
-            "input and filter must resolve to the same strong dtype. input is"
-            f" {x.dtype}. filter is {filter.dtype}."
-        )
-
     if bias is not None:
         x, bias = dtype_promotion._promote_weak_dtypes(x, bias)
         if x.dtype != bias.dtype:
@@ -191,32 +247,81 @@ def conv3d(
                 "input and bias must resolve to the same strong dtype. input is"
                 f" {x.dtype}. bias is {bias.dtype}."
             )
-
         if bias.rank != 1:
             raise ValueError(
-                "bias for a 2-D convolution must be rank 1 with shape (out_channels,)"
+                "bias for a 3-D convolution must be rank 1 with shape (out_channels,)"
             )
-
+    if x.dtype != filter.dtype:
+        raise ValueError(
+            "input and filter must resolve to the same strong dtype. input is"
+            f" {x.dtype}. filter is {filter.dtype}."
+        )
     if x.rank != 5:
         raise ValueError(
             "input to a 3-D convolution must be rank 5 with shape (batch_size,"
             " depth, height, width, in_channels)"
         )
-
     if filter.rank != 5:
         raise ValueError(
             "filter for a 3-D convolution must be rank 5 with shape (depth,"
             " height, width, in_channels / num_groups, out_channels)"
         )
+    if dilation != (1, 1, 1):
+        raise ValueError("Non-unit dilation is not implemented yet!")
+
+    stride_constant = constant(
+        np.array([stride[0], stride[1], stride[2]]), DType.int64
+    )
+
+    dilation_constant = constant(
+        np.array([dilation[0], dilation[1], dilation[2]]), DType.int64
+    )
+
+    padding_constant = constant(
+        np.array(
+            [
+                padding[0],
+                padding[1],
+                padding[2],
+                padding[3],
+                padding[4],
+                padding[5],
+            ]
+        ),
+        DType.int64,
+    )
+
+    groups_constant = constant(groups, DType.int64)
+
+    # TODO(MSDK-1196): create rmo.conv and move this to rmo
+    output_shape = x.shape
+
+    input_depth, filter_depth = x.shape[1], filter.shape[0]
+    output_shape[1] = (
+        input_depth - filter_depth + padding[0] + padding[1] + stride[0]
+    ) // stride[0]
+
+    input_height, filter_height = x.shape[2], filter.shape[1]
+    output_shape[2] = (
+        input_height - filter_height + padding[2] + padding[3] + stride[1]
+    ) // stride[1]
+
+    input_width, filter_width = x.shape[3], filter.shape[2]
+    output_shape[3] = (
+        input_width - filter_width + padding[4] + padding[5] + stride[2]
+    ) // stride[2]
+
+    output_shape[4] = filter.shape[4]
 
     conv_output = Graph.current._add_op(
-        rmo.conv,
-        TensorValue(x),
-        TensorValue(filter),
-        Shape(stride).to_mlir(),
-        Shape(dilation).to_mlir(),
-        Shape(padding).to_mlir(),
-        groups,
+        rmo.mo_conv,
+        TensorType(x.dtype, output_shape, x.device).to_mlir(),
+        x,
+        filter,
+        stride_constant,
+        dilation_constant,
+        padding_constant,
+        groups_constant,
     )[0].tensor
 
     if bias is not None:
