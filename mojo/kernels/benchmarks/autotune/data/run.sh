a3df9bbd5bbeaef45afd3ad77b5c0d49833f5727
Joe Loser <joe@modular.com>
2025-04-21 21:10:29 -0600

    [Kernels] Update licences (#60148)
    As agreed upon today, update all of the files in `Kernels` directory to
    have the Apache2 license with LLVM Exceptions, matching that of the rest
    of the Mojo standard library.
    
    Exclude `Kernels/benchmarks/external_benchmarks` as mentioned in
    `utils/lint-copyright-notices.py` for consistency for now.
    
    KERNEL_BENCHMARKS_ORIG_REV_ID: 6ed474ef8076f08bf06a37211a55a8c0de24e235

--- mojo/kernels/benchmarks/autotune/data/run.sh
+++ mojo/kernels/benchmarks/autotune/data/run.sh
@@ -1,7 +1,14 @@
 ##===----------------------------------------------------------------------===##
+# Copyright (c) 2025, Modular Inc. All rights reserved.
 #
-# This file is Modular Inc proprietary.
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 ##===----------------------------------------------------------------------===##
 #!/bin/bash
 
@@ -14,12 +21,12 @@ cd $dir
 kplot output_base.csv output_branch.csv -o base_vs_branch
 kplot output_base.csv output_branch.csv -o base_vs_branch -x pdf
 kplot output_base.csv output_branch.csv -o base_vs_branch_label \
-	--label="baseline" --label="experiment"
+  --label="baseline" --label="experiment"
 kplot output_base.csv output_branch.csv -o base_vs_branch_label \
-	--label="baseline" --label="experiment" -x pdf
+  --label="baseline" --label="experiment" -x pdf
 kplot output_base.csv output_branch.csv -o base_vs_branch_label_compare \
-	--label="baseline" --label="experiment" -c
+  --label="baseline" --label="experiment" -c
 kplot output_base.csv output_branch.csv output_cublas.csv -o base_vs_branch_vs_cublas \
-	--label="baseline" --label="experiment" --label="cublas" -f
+  --label="baseline" --label="experiment" --label="cublas" -f
 kplot output_base.csv output_branch.csv output_cublas.csv -o base_vs_branch_vs_cublas \
-	--label="baseline" --label="experiment" --label="cublas" -f -x pdf
+  --label="baseline" --label="experiment" --label="cublas" -f -x pdf
