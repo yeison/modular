39cdfb7ca08dfc6eef6632b5954ab0b89acf5bad
Andrew Luo <andrew.zhao.luo@gmail.com>
2025-05-07 14:22:10 -0700

    [Graph API] Correct Device Assignment in Sampling
    As part of explicit devices, we no longer automatically fixup device
    errors with transfers, so correct things in sampling.
    
    MODULAR_ORIG_COMMIT_REV_ID: 4bd778351d82709a13f9cf697b6745cfafbeb735

--- max/nn/sampling/rejection_sampler.py
+++ max/nn/sampling/rejection_sampler.py
@@ -118,8 +118,12 @@ class RejectionSampler(nn.Module):
         sampled_target_tokens = ops.custom(
             "topk_fused_sampling",
             [
-                ops.constant(self.top_k, dtype=DType.int64, device=self.device),
-                ops.gather(target_logits, rejected_offsets, axis=0),
+                ops.constant(
+                    self.top_k, dtype=DType.int64, device=DeviceRef.CPU()
+                ),
+                ops.gather(target_logits, rejected_offsets, axis=0).to(
+                    DeviceRef.CPU()
+                ),
             ],
             [
                 TensorType(
