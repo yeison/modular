b024679a20d92ab01d5dff272429183bc38b868d
Spenser Bauman <spenser@modular.com>
2025-05-13 18:41:08 +0000

    [PyTorch] Rename max.torch to max.torch_legacy
    The existing max.torch tooling will soon be deprecated. In order to make
    way for a new integration pathway, rename max.torch to max.torch_legacy
    to clear up the namespace for the new implementation.
    
    MODULAR_ORIG_COMMIT_REV_ID: 3a9f9df9fdc2fc9322f630c2a67fe99156fc6d5a

--- examples/custom_ops/whisper.py
+++ examples/custom_ops/whisper.py
@@ -19,7 +19,7 @@ import sysconfig
 from pathlib import Path
 from typing import Optional
 
-import max.torch as mtorch
+import max.torch_legacy as mtorch
 import torch
 import transformers
 from datasets import load_dataset
