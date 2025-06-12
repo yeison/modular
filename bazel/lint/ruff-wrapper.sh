c002c8aef4ebc190ae3823ecd7eb74489b422ed3
Alex Trotta <44127594+Ahajha@users.noreply.github.com>
2025-06-12 19:09:05 -0400

    [bazel] Add ruff linters to `br //:lint` and `br //:format`
    MODULAR_ORIG_COMMIT_REV_ID: 8e015763d0384e895b3c8d998a8c54f4919cdd8c

---
+++ bazel/lint/ruff-wrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+# Copyright (c) 2025, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##===----------------------------------------------------------------------===##
+
+set -euo pipefail
+
+binary_root=$PWD/..
+
+cd "$BUILD_WORKSPACE_DIRECTORY"
+
+binary=$(find $binary_root -name ruff | head -n 1)
+exec "$binary" "$@"
