[Pipelines] Fix repetition penalty calculation
We should use `context.all_tokens`, which includes all prompt and
generated tokens. `context.tokens` contains padding tokens.

MODULAR_ORIG_COMMIT_REV_ID: 690c82200a2b69e2a7bf5be5de748608b3fc7d1c

max/pipelines/lib/pipeline.py
@@ -740,7 +740,9 @@ class TextGenerationPipeline(TokenGenerator[T]):
         tracer.next("build_token_frequency_csr_loop")
         for i, context in enumerate(batch):
             unique_tokens, counts = np.unique(
-                context.tokens if include_prompt else context.generated_tokens,
+                context.all_tokens
+                if include_prompt
+                else context.generated_tokens,
                 return_counts=True,
             )
             # Pad the tokens and counts to reserve space for new tokens