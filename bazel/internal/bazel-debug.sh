acee3cd8454dd0a8206065abfd2885c991fb57b5
Keith Smiley <keithbsmiley@gmail.com>
2025-07-10 16:32:38 -0700

    [bazel] Open source bazel-debug script
    This is useful for attaching lldb in complex situations. Not all cases
    work externally yet but more will work as we open source more configs
    
    MODULAR_ORIG_COMMIT_REV_ID: 4e8192ff220fea86031c9a2ab31d2688d2e62f87

---
+++ bazel/internal/bazel-debug.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+# Copyright (c) 2025, Modular Inc. All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions:
+# https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##===----------------------------------------------------------------------===##
+
+set -euo pipefail
+
+# Test cases:
+#
+# - cc_binary: //KGEN/tools/mojo -- -h
+# - cc_test: //KGEN/unittests
+# - cc_test's underlying cc_binary: //KGEN/unittests:unittests.debug
+# - py_binary: //SDK/lib/API/python/max/entrypoints:pipelines -- generate --model-path modularai/Llama-3.1-8B-Instruct-GGUF --prompt "I believe the meaning of life is" --max-new-tokens 8 --max-batch-size 4 --cache-strategy=continuous --quantization-encoding float32
+# - py_test: Support/python:unittests
+# - mojo_binary: Kernels/test/gpu-query
+# - mojo_test: open-source/max/mojo/stdlib/test:builtin/test_math.mojo.test
+# - arg parsing: --config=debug-bazel Support/python:unittests --config=debug-bazel -- other args
+
+target=""
+default_config="lldb"
+before_target=()
+vscode=0
+gdb=0
+system_lldb=0
+for arg in "$@"
+do
+  if [[ "$arg" != -* ]]; then
+    target="$arg"
+    shift
+    break
+  elif [[ "$arg" = --vscode ]]; then
+    vscode=1
+    shift
+  elif [[ "$arg" = --gdb ]]; then
+    gdb=1
+    shift
+  elif [[ "$arg" = --system-lldb ]]; then
+    system_lldb=1
+    shift
+  else
+    before_target+=("$arg")
+    shift
+  fi
+done
+
+wrapper="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/../bazelw"
+# py_binary rule //path/to:target
+output=$("$wrapper" query "some(labels(actual, $target) union set($target))" --output=label_kind)
+kind="${output%% *}"
+# Resolve target shorthand and aliases through single query
+target=${output##* }
+
+subcommand="run"
+run_debug_script=false
+if [[ "$kind" == py_binary ]]; then
+  # py_binary has an underlying py_repl that we can use for debugging
+  target="$target.debug"
+elif [[ "$kind" == py_test ]]; then
+  # py_test has a custom codepath to dump a debugging script, which should be run after
+  run_debug_script=true
+  subcommand="test"
+  default_config="debug-pytest"
+  before_target+=("//bazel:lldb_wrapper")
+elif [[ "$kind" == binary_test ]]; then
+  # cc_test has an underlying binary that is debuggable with bazel run
+  target="$target.debug"
+elif [[ "$kind" == mojo_test ]]; then
+  # mojo_test has an underyling mojo_binary that is debuggable with bazel run
+  target="$target.debug"
+elif [[ "$kind" == modular_genrule ]]; then
+  echo "error: modular_genrule not currently supported for debugging" >&2
+  exit 1
+fi
+
+before_target+=("--config=$default_config")
+MODULAR_SYSTEM_LLDB="$system_lldb" MODULAR_GDB="$gdb" MODULAR_VSCODE_DEBUG="$vscode" "$wrapper" "$subcommand" "${before_target[@]}" "$target" "$@"
+if [[ "$run_debug_script" == "true" ]]; then
+  MODULAR_SYSTEM_LLDB="$system_lldb" MODULAR_GDB="$gdb" MODULAR_VSCODE_DEBUG="$vscode" exec /tmp/lldb.sh
+fi
