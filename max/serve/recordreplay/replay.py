[RecordReplay] Fix truncated normal computation
I made a mistake in the formula.  Correct it.

MODULAR_ORIG_COMMIT_REV_ID: 1fc6c9491d90dfb91236124aea6204a997e4fe09

max/serve/recordreplay/replay.py
@@ -105,7 +105,7 @@ class _IncrementalDistributionComputer:
 def _tnmean(a: float) -> float:
     """Mean of the standard normal distribution, truncated to (a, +∞)."""
     # Implementation ported from https://github.com/cossio/TruncatedNormal.jl.
-    return math.sqrt(2 / math.pi) * scipy.special.erfcx(a / math.sqrt(2))
+    return math.sqrt(2 / math.pi) / scipy.special.erfcx(a / math.sqrt(2))
 
 
 def _low_truncated_expectation(