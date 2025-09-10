8f4ecabf0306e54086537ff86ae1e25664e43318
Austin Doolittle <austin@modular.com>
2025-09-10 12:12:17 -0700

    [Pipelines] NFC remove dead code
    MODULAR_ORIG_COMMIT_REV_ID: 9347f45848fd8eb71e47afa86d55c448f9fdcd11

--- max/pipelines/lib/memory_estimation.py
+++ max/pipelines/lib/memory_estimation.py
@@ -195,31 +195,6 @@ class MemoryEstimator:
             )
             total_size = model_weights_size + actual_kv_cache_size
 
-        if free_memory:
-            free_memory_str = f" / {to_human_readable_bytes(free_memory)} free"
-
-        weights_str = ""
-        if model_weights_size:
-            weights_str = f"\n\t    Weights:                {to_human_readable_bytes(model_weights_size)}"
-
-        activation_str = ""
-        if activation_memory_size:
-            activation_str = f"\n\t    Activation memory:      {to_human_readable_bytes(activation_memory_size)}"
-
-        if not user_provided_max_length:
-            max_length_str = f"Auto-inferred max sequence length: {pipeline_config.max_length}"
-        else:
-            max_length_str = (
-                f"Current max sequence length: {pipeline_config.max_length}"
-            )
-
-        if not user_provided_max_batch_size:
-            max_batch_size_str = f"Auto-inferred max batch size: {pipeline_config.max_batch_size}"
-        else:
-            max_batch_size_str = (
-                f"Current max batch size: {pipeline_config.max_batch_size}"
-            )
-
         vram_usage_limit_scale = 0.95
 
         if isinstance(free_memory, (int, float)):
